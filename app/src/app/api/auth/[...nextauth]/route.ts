8f18f3a9b06d92c4e4cb28ca159091838e3c072e
timthedev07 <timpersonal07@gmail.com>
2023-09-30 15:53:11 +0200

    use custom adapter

--- app/src/app/api/auth/[...nextauth]/route.ts
+++ app/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "../../../../lib/prismaAdapter";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -15,6 +15,7 @@ const handler = NextAuth({
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
+      // only allow internal users
       if (
         !(
           user.email?.endsWith("@kcpupils.org") &&
@@ -27,6 +28,9 @@ const handler = NextAuth({
       }
 
       const u = await prisma.user.findFirst({ where: { email: user.email } });
+
+      // forbid blocked users
+      if (!u || u.blocked) return false;
 
       return process.env.NODE_ENV === "development";
     },
