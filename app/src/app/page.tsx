5b533654be22eb8aecea850b0b7210cab5145e18
timthedev07 <timpersonal07@gmail.com>
2023-09-30 17:28:30 +0200

    test login

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
 export default function Home() {
-  return "HI";
+  return (
+    <div className="p-64">
+      <button
+        onClick={() => {
+          signIn("google");
+        }}
+        className="px-4 py-2 flex justify-center text-black items-center font-bold rounded-md bg-slate-300"
+      >
+        SIGN IN
+      </button>
+    </div>
+  );
 }
