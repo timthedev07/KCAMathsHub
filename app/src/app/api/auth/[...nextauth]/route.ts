bed93360c32c6aab9647cdf4556c374d3948cc4d
timthedev07 <timpersonal07@gmail.com>
2023-09-30 13:18:00 +0200

    nextjs init migrations

--- app/src/app/api/auth/[...nextauth]/route.ts
+++ app/src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
