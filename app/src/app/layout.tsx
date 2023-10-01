e3b138353eb13f4dcc0d88622f71c6e29f46720c
timthedev07 <timpersonal07@gmail.com>
2023-10-01 16:25:08 +0200

    use trpc client provider

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { TRPCProvider } from "../trpc/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className + ` bg-primary-bg text-text-color`}>
-        {children}
+        <TRPCProvider>{children}</TRPCProvider>
       </body>
     </html>
   );
