d7d90828a621b61729ecfca0dc2464b9b88d7048
timthedev07 <timpersonal07@gmail.com>
2023-10-01 18:20:27 +0200

    update env types

--- app/env.d.ts
+++ app/env.d.ts
@@ -2,8 +2,13 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NEXT_AUTH_SECRET: string;
+      DATABASE_URL: string;
       GOOGLE_CLIENT_ID: string;
       GOOGLE_CLIENT_SECRET: string;
+      AWS_ACCESS_KEY_ID: string;
+      AWS_SECRET_ACCESS_KEY: string;
+      AWS_REGION: string;
+      S3_BUCKET_NAME: string;
     }
   }
 }
