a4c6633161ddbd15f47a1621c11a1e2e0511f29b
timthedev07 <timpersonal07@gmail.com>
2023-10-05 13:04:25 +0200

    include react dev tools

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { TRPCProvider } from "../trpc/Provider";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script src="http://localhost:8097"></Script>
       <body className={inter.className + ` bg-primary-bg text-text-color`}>
         <TRPCProvider>{children}</TRPCProvider>
       </body>
