c62a380433fc333dbe7ac2ade81de0afa92cf5ad
timthedev07 <timpersonal07@gmail.com>
2023-10-05 22:51:30 +0200

    include new mutation

--- app/src/server/index.ts
+++ app/src/server/index.ts
@@ -1,8 +1,10 @@
 import { router } from "./trpc";
 import { updateUsername } from "./crud/initUsername";
+import { addAttachments } from "./crud/addAttachment";
 
 export const appRouter = router({
   updateUsername,
+  addAttachments,
 });
 
 export type AppRouter = typeof appRouter;
