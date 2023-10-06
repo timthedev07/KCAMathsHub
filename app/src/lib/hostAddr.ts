c032de54ba34c6633d46fc967a303bcfca3f8348
timthedev07 <timpersonal07@gmail.com>
2023-10-07 01:09:26 +0200

    fix

--- app/src/lib/hostAddr.ts
+++ app/src/lib/hostAddr.ts
@@ -1,5 +1,5 @@
 export const PROD_HOST = "https://kcamathshub.vercel.app";
-export const DEV_HOST = "localhost:3000";
+export const DEV_HOST = "http://localhost:3000";
 
 export const HOST =
   process.env.NODE_ENV === "development" ? DEV_HOST : PROD_HOST;
