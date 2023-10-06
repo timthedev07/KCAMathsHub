4a08555902e3197f18f4679b5fdc81dc9460dab4
timthedev07 <timpersonal07@gmail.com>
2023-10-07 01:10:34 +0200

    include query

--- app/src/server/index.ts
+++ app/src/server/index.ts
@@ -2,11 +2,13 @@ import { router } from "./trpc";
 import { updateUsername } from "./crud/user/initUsername";
 import { addAttachments } from "./crud/questions/addAttachment";
 import { initReferral } from "./crud/user/initReferral";
+import { getReferralId } from "./crud/user/getReferralId";
 
 export const appRouter = router({
   updateUsername,
   addAttachments,
   initReferral,
+  getReferralId,
 });
 
 export type AppRouter = typeof appRouter;
