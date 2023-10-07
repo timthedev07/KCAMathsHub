134593b635a3ed1f74c455605ba358f105b48380
timthedev07 <timpersonal07@gmail.com>
2023-10-07 12:28:18 +0200

    increment reputation

--- app/src/server/crud/user/acceptReferral.ts
+++ app/src/server/crud/user/acceptReferral.ts
@@ -3,6 +3,8 @@ import { publicProcedure } from "../../trpc";
 import prisma from "../../../db";
 import { TRPCError } from "@trpc/server";
 
+export const REFERRAL_REP_GAIN = 20;
+
 export const acceptReferral = publicProcedure
   .input(
     z.object({
@@ -11,12 +13,25 @@ export const acceptReferral = publicProcedure
     })
   )
   .mutation(async ({ input: { referralId, userId } }) => {
-    if (!userId) {
+    if (!(await prisma.user.findFirst({ where: { id: userId } }))) {
       throw new TRPCError({ code: "UNAUTHORIZED" });
     }
+    const creator = await prisma.user.findFirst({ where: { referralId } }); // find the user who created the referral
+    if (!creator) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Referral not found",
+      });
+    }
+
     await prisma.referral.update({
       where: { id: referralId },
       data: { acceptedUsers: { connect: { id: userId } } },
+    });
+
+    await prisma.user.update({
+      where: { id: creator.id },
+      data: { answererReputation: { increment: REFERRAL_REP_GAIN } },
     });
     return true;
   });
