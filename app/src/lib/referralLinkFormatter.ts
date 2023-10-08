cbc784f73edafee2ce2cd0a5f898450d7f8f7d1d
timthedev07 <timpersonal07@gmail.com>
2023-10-08 14:13:39 +0200

    update referral link

--- app/src/lib/referralLinkFormatter.ts
+++ app/src/lib/referralLinkFormatter.ts
@@ -1,5 +1,5 @@
 import { HOST } from "./hostAddr";
 
 export const getReferralLink = (referralId: string) => {
-  return `${HOST}/referral/${referralId}`;
+  return `${HOST}/account/signin?r=${referralId}`;
 };
