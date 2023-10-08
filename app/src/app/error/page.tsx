9d191b3ec95cd33c35da347f8408b3983bdd9df3
timthedev07 <timpersonal07@gmail.com>
2023-10-08 22:02:42 +0200

    custom err page

--- app/src/app/error/page.tsx
+++ app/src/app/error/page.tsx
@@ -1,16 +1,34 @@
 "use client";
 import { NextPage } from "next";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Button } from "../../components/reusable/Button";
 
 const ErrorPage: NextPage = () => {
   const { get } = useSearchParams();
+  const { back, push } = useRouter();
 
   const err = get("err") || "Unknown error...";
 
   return (
-    <div className="w-full h-[90vh] flex justify-center items-center flex-col">
-      <h1 className="text-2xl text-red-600 font-bold">Blunder...</h1>
-      <p className="text-red-600/70 font-medium">{err}</p>
+    <div className="w-full h-[90vh] flex justify-center items-center flex-col gap-8">
+      <h1 className="text-8xl text-red-400 font-bold">Blunder...</h1>
+      <p className="text-red-400/80 font-medium">{err}</p>
+      <div className="flex justify-between w-full max-w-[300px]">
+        <Button
+          onClick={() => {
+            back();
+          }}
+        >
+          Go back
+        </Button>
+        <Button
+          onClick={() => {
+            push("/");
+          }}
+        >
+          Home
+        </Button>
+      </div>
     </div>
   );
 };
