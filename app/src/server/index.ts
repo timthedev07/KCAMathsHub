9bd68f1a2d25706e8a968c46a44914224e3a9649
timthedev07 <timpersonal07@gmail.com>
2023-10-08 15:10:30 +0200

    include accept referral mutation

--- app/src/server/index.ts
+++ app/src/server/index.ts
@@ -3,12 +3,14 @@ import { updateUsername } from "./crud/user/initUsername";
 import { addAttachments } from "./crud/questions/addAttachment";
 import { initReferral } from "./crud/user/initReferral";
 import { getReferralId } from "./crud/user/getReferralId";
+import { acceptReferral } from "./crud/user/acceptReferral";
 
 export const appRouter = router({
   updateUsername,
   addAttachments,
   initReferral,
   getReferralId,
+  acceptReferral,
 });
 
 export type AppRouter = typeof appRouter;
