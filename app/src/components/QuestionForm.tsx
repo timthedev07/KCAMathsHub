bc3701e0bfb4ec67a21b5a387772f7905dd9de04
timthedev07 <timpersonal07@gmail.com>
2023-10-08 22:02:49 +0200

    init question form component

---
+++ app/src/components/QuestionForm.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+interface QuestionFormProps {}
+
+export const QuestionForm: FC<QuestionFormProps> = ({}) => {
+  return <form className="dev-orange-border"></form>;
+};
