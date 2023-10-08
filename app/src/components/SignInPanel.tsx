b11bce4d9df7980d77e23bc2e33cf4edbc7fa043
timthedev07 <timpersonal07@gmail.com>
2023-10-08 15:31:02 +0200

    extract client-side sign in logic to component

---
+++ app/src/components/SignInPanel.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import { FC } from "react";
+import { OAuthButton } from "react-auth-provider-buttons";
+import { HOST } from "../lib/hostAddr";
+
+interface SignInPanelProps {
+  r?: string | null; // from /account/signin/[r]
+}
+
+export const SignInPanel: FC<SignInPanelProps> = ({ r }) => {
+  return (
+    <div>
+      <OAuthButton
+        provider="google"
+        onClick={async () => {
+          if (r)
+            await signIn("google", {
+              callbackUrl: `${HOST}/account/signin/success=true&r=${r}`,
+            });
+          else await signIn("google", { callbackUrl: "/" });
+        }}
+      />
+    </div>
+  );
+};
