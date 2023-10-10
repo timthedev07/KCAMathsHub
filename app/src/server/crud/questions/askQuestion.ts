cd50e2577d81f43e3160aa4e7dab42dcf1c335e3
timthedev07 <timpersonal07@gmail.com>
2023-10-11 00:17:45 +0200

    init ask question api

---
+++ app/src/server/crud/questions/askQuestion.ts
@@ -0,0 +1,61 @@
+import { z } from "zod";
+import { publicProcedure } from "../../trpc";
+import prisma from "../../../db";
+import { TRPCError } from "@trpc/server";
+
+export const TITLE_LIMIT = [8, 128];
+export const CONTENT_LIMIT = [50, 5000];
+
+export const askQuestion = publicProcedure
+  .input(
+    z.object({
+      title: z
+        .string()
+        .min(TITLE_LIMIT[0], "Title is not long enough")
+        .max(TITLE_LIMIT[1], "Title too long"),
+      content: z
+        .string()
+        .min(
+          CONTENT_LIMIT[0],
+          "Please write more than 50 characters as the question content"
+        )
+        .max(CONTENT_LIMIT[1], "Too many words for the content"),
+      userId: z.string().nonempty("Invalid user"),
+      attachmentIds: z.number().array(),
+    })
+  )
+  .mutation(async ({ input: { attachmentIds, content, title, userId } }) => {
+    const u = await prisma.user.findFirst({ where: { id: userId } });
+    if (!u)
+      throw new TRPCError({ code: "UNAUTHORIZED", message: "Invalid user" });
+
+    const attachments = await prisma.imageAttachment.findMany({
+      where: { id: { in: attachmentIds } },
+    });
+
+    if (attachmentIds.length !== attachments.length) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Invalid attachments",
+      });
+    }
+
+    try {
+      const askedQuestion = await prisma.question.create({
+        data: {
+          content,
+          title,
+          attachments: {
+            connect: attachments,
+          },
+          questioner: { connect: { id: userId } },
+        },
+      });
+      return askedQuestion.id;
+    } catch (err: any) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: err.message,
+      });
+    }
+  });
