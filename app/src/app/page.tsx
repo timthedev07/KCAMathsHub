e6c434a37c91062aa80b2d7c34b9420ed09b3a2c
timthedev07 <timpersonal07@gmail.com>
2023-10-12 01:41:29 +0200

    responsive

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -10,14 +10,13 @@ const Home = async () => {
   const uid = session?.user.id;
 
   return (
-    <div className="p-64 flex flex-col gap-12">
+    <div className="p-8 sm:p-24 md:p-64 flex flex-col gap-12">
       <SignInButton signedIn={!!session?.user} />
       <div className="dev-border-red flex flex-col gap-4 p-12">
         <Link href="/account/signin">Sign In</Link>
         <Link href="/account/complete-signup">Complete Sign Up</Link>
         <Link href="/account/profile">Profile</Link>
       </div>
-      {}
       {!!uid ? (
         <>
           <QuestionForm userId={uid} />
