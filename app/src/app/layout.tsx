4771aae043473cad73f89a8eaf1ddbee48a5ece9
timthedev07 <timpersonal07@gmail.com>
2023-10-12 01:48:34 +0200

    clean up

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -2,8 +2,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { TRPCProvider } from "../trpc/Provider";
-import Script from "next/script";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
