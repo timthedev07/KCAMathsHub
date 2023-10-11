47f4d18f6c751d3a8d693bd6945e02d51c022a3d
timthedev07 <timpersonal07@gmail.com>
2023-10-12 01:29:22 +0200

    extract client upload logic

---
+++ app/src/lib/attachmentClientUpload.ts
@@ -0,0 +1,34 @@
+"use client";
+
+import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import { FileWithIdAndObjURL } from "../components/AttachmentUpload";
+import { AppRouter } from "../server";
+import { ImgUrlsType } from "../types/upload";
+
+export const uploadToAPI = async (
+  files: FileWithIdAndObjURL[],
+  addAttachments: (
+    v: inferRouterInputs<AppRouter>["addAttachments"]
+  ) => Promise<inferRouterOutputs<AppRouter>["addAttachments"]>
+) => {
+  if (files.length < 1) return;
+
+  const formData = new FormData();
+  let i = 0;
+  for (const file of files) {
+    formData.append(`file_${i}`, file.file);
+    i++;
+  }
+  formData.append("file_count", `${i}`);
+
+  const res = await fetch("/api/upload", {
+    body: formData,
+    method: "POST",
+  });
+
+  const data = ((await res.json()) as { imgUrls: ImgUrlsType }).imgUrls;
+  const atts = await addAttachments(
+    data.map(({ name, url }) => ({ url: url, attachmentName: name }))
+  );
+  return atts;
+};
