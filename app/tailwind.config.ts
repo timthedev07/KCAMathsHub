52bcee37247b12ceab5032299a5a2abcb1607c24
timthedev07 <timpersonal07@gmail.com>
2023-10-11 11:44:27 +0200

    include reusable classnames

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/reusable-vars/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   safelist: ["bg-emerald-600", "bg-cyan-600", "bg-orange-600", "bg-red-600"], // these are for dev buttons, may be unnecessary in prod
   theme: {
