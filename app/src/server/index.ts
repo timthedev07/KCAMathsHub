6230dd517d07f2b5c3e407f363c936437ba314c3
timthedev07 <timpersonal07@gmail.com>
2023-10-12 15:38:31 +0200

    include query

--- app/src/server/index.ts
+++ app/src/server/index.ts
@@ -5,6 +5,7 @@ import { initReferral } from "./crud/user/initReferral";
 import { getReferral } from "./crud/user/getReferralId";
 import { askQuestion } from "./crud/questions/askQuestion";
 import { getQuestion } from "./crud/questions/getQuestion";
+import { getQuestions } from "./crud/questions/getQuestions";
 
 export const appRouter = router({
   updateUsername,
@@ -13,6 +14,7 @@ export const appRouter = router({
   getReferral,
   askQuestion,
   getQuestion,
+  getQuestions,
 });
 
 export type AppRouter = typeof appRouter;
