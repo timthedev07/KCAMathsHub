34910e379da5ae21348a1b00111d36812d610b7b
timthedev07 <timpersonal07@gmail.com>
2023-10-12 14:05:38 +0200

    fixes #42

--- app/src/server/crud/questions/askQuestion.ts
+++ app/src/server/crud/questions/askQuestion.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import { publicProcedure } from "../../trpc";
 import prisma from "../../../db";
 import { TRPCError } from "@trpc/server";
+import { classifyKSCategory } from "../../../lib/classifyKSCategory";
 
 export const TITLE_LIMIT = [8, 128];
 export const CONTENT_LIMIT = [50, 5000];
@@ -26,8 +27,16 @@ export const askQuestion = publicProcedure
   )
   .mutation(async ({ input: { attachmentIds, content, title, userId } }) => {
     const u = await prisma.user.findFirst({ where: { id: userId } });
+
     if (!u)
       throw new TRPCError({ code: "UNAUTHORIZED", message: "Invalid user" });
+
+    if (!u.joinedYear) {
+      throw new TRPCError({
+        code: "FORBIDDEN",
+        message: "At this stage, only students can ask questions.",
+      });
+    }
 
     const attachments = await prisma.imageAttachment.findMany({
       where: { id: { in: attachmentIds } },
@@ -52,6 +61,7 @@ export const askQuestion = publicProcedure
             connect: attachments,
           },
           questioner: { connect: { id: userId } },
+          category: classifyKSCategory(u.joinedDate, u.joinedYear),
         },
       });
       finalId = askedQuestion.id;
