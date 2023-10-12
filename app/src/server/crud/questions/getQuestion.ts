eb154a20e00f8fed6ca7ece530f92d8033ba000e
timthedev07 <timpersonal07@gmail.com>
2023-10-12 12:37:43 +0200

    get question query

---
+++ app/src/server/crud/questions/getQuestion.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+import { publicProcedure } from "../../trpc";
+import prisma from "../../../db";
+import { TRPCError } from "@trpc/server";
+
+const userSelection = { username: true, image: true, id: true };
+const attachmentSelection = { name: true, imgUrl: true };
+
+export const getQuestion = publicProcedure
+  .input(
+    z.object({
+      quid: z.string(),
+    })
+  )
+  .mutation(async ({ input: { quid } }) => {
+    const q = await prisma.question.findUnique({
+      where: { id: quid },
+      include: {
+        questioner: { select: userSelection },
+        answer: {
+          select: {
+            answerer: { select: userSelection },
+            content: true,
+            helpful: true,
+            attachments: { select: attachmentSelection },
+            moderated: true,
+            moderations: {
+              select: {
+                moderator: { select: userSelection },
+                approval: true,
+                timestamp: true,
+                moderationComment: true,
+              },
+            },
+          },
+        },
+        attachments: { select: attachmentSelection },
+      },
+    });
+    return q;
+  });
--- app/src/server/index.ts
+++ app/src/server/index.ts
@@ -4,6 +4,7 @@ import { addAttachments } from "./crud/questions/addAttachment";
 import { initReferral } from "./crud/user/initReferral";
 import { getReferral } from "./crud/user/getReferralId";
 import { askQuestion } from "./crud/questions/askQuestion";
+import { getQuestion } from "./crud/questions/getQuestion";
 
 export const appRouter = router({
   updateUsername,
@@ -11,6 +12,7 @@ export const appRouter = router({
   initReferral,
   getReferral,
   askQuestion,
+  getQuestion,
 });
 
 export type AppRouter = typeof appRouter;
