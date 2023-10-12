fe93da270fb82a7df8b89cd6573c2444872e49fd
timthedev07 <timpersonal07@gmail.com>
2023-10-12 15:37:24 +0200

    resolves #10

---
+++ app/src/server/crud/questions/getQuestions.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import { publicProcedure } from "../../trpc";
+import prisma from "../../../db";
+import { StudentStages } from "../../../types/StudentStage";
+import { TRPCError } from "@trpc/server";
+
+export const getQuestions = publicProcedure
+  .input(
+    z.object({
+      category: z
+        .enum([...StudentStages, "all"])
+        .optional()
+        .default("all"),
+      limit: z.number().max(50).min(10).optional().default(20),
+      pageNum: z.number().min(1),
+      sortBy: z.enum(["timestamp"]).optional().default("timestamp"),
+      order: z.enum(["asc", "desc"]).optional().default("desc"),
+    })
+  )
+  .query(async ({ input: { category, limit, pageNum, sortBy, order } }) => {
+    try {
+      return await prisma.question.findMany({
+        skip: limit * (pageNum - 1),
+        take: limit,
+        where: {
+          category,
+        },
+        orderBy: {
+          [sortBy]: order,
+        },
+      });
+    } catch (err: any) {
+      console.log(err);
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: err?.message || "Unknown error.",
+      });
+    }
+  });
