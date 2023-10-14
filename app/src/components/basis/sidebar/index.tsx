cb8031fa144c3938d0ae2ebe8f43757f1fea18c1
timthedev07 <timpersonal07@gmail.com>
2023-10-14 17:16:12 +0200

    sidebar with items

--- app/src/components/basis/sidebar/index.tsx
+++ app/src/components/basis/sidebar/index.tsx
@@ -1,4 +1,9 @@
 import { FC } from "react";
+import { Item } from "./Item";
+import { HomeSVG } from "../../../svgs/sidebar/Home";
+import { AccountSVG } from "../../../svgs/sidebar/Account";
+import { signOut } from "next-auth/react";
+import { LogoutSVG } from "../../../svgs/sidebar/Logout";
 
 interface SidebarProps {
   open: boolean;
@@ -13,16 +18,23 @@ export const SideBar: FC<SidebarProps> = ({ open, bg, transDuration }) => {
         open
           ? "translate-x-0 ease-out opacity-100"
           : "-translate-x-full ease-in opacity-0"
-      } px-8 py-8 overflow-hidden row-start-2 row-end-3 col-start-1 col-end-3 overflow-y-auto z-10 scrollbar-in-light-bg`}
+      } px-5 py-8 overflow-hidden row-start-2 row-end-3 col-start-1 col-end-3 overflow-y-auto z-10 scrollbar-in-light-bg`}
     >
       <ul
         className={`transition ${transDuration} ${
           open ? "blur-none" : "blur-sm"
-        }`}
+        } flex flex-col gap-4`}
       >
-        {Array.from(Array(10).keys()).map((each) => {
-          return <li key={each}>SCROLL ME</li>;
-        })}
+        <Item action="/" text="Home" Icon={HomeSVG} />
+        <Item action="/account" text="Account" Icon={AccountSVG} />
+        <hr className="opacity-50 bg-slate-600 border-slate-600" />
+        <Item
+          action={async () => {
+            await signOut();
+          }}
+          text="Log out"
+          Icon={LogoutSVG}
+        />
       </ul>
     </aside>
   );
