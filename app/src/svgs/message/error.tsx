a48bc8eb6c2f7ec7232249a06d68e0e5b4432fee
timthedev07 <timpersonal07@gmail.com>
2023-11-05 23:20:17 +0100

    svgs

---
+++ app/src/svgs/message/error.tsx
@@ -0,0 +1,13 @@
+import { forwardRef, SVGProps } from "react";
+
+export const ErrorSVG = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
+  ({ ...props }, ref) => {
+    return (
+      <svg ref={ref} viewBox="0 0 512 512" {...props}>
+        <path d="M256 512a256 256 0 1 0 0-512 256 256 0 1 0 0 512zm-81-337c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
+      </svg>
+    );
+  }
+);
+
+ErrorSVG.displayName = "error";
---
+++ app/src/svgs/message/success.tsx
@@ -0,0 +1,13 @@
+import { forwardRef, SVGProps } from "react";
+
+export const SuccessSVG = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
+  ({ ...props }, ref) => {
+    return (
+      <svg ref={ref} viewBox="0 0 512 512" {...props}>
+        <path d="M256 512a256 256 0 1 0 0-512 256 256 0 1 0 0 512zm113-303L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
+      </svg>
+    );
+  }
+);
+
+SuccessSVG.displayName = "Success";
