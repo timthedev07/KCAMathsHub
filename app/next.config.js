2a216f7c6c642d80b91da8c1d47bde11c6961dcc
timthedev07 <timpersonal07@gmail.com>
2023-11-24 10:49:41 +0100

    fix

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
+    remotePatterns: [
       `${process.env.S3_BUCKET_NAME}.s3-${process.env.AWS_REGION}.amazonaws.com/`,
       "placehold.co",
       "lh3.googleusercontent.com",
