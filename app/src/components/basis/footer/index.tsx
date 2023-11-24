69a1c84566c69b4166173324b358f739c77143b5
timthedev07 <timpersonal07@gmail.com>
2023-11-24 12:42:03 +0100

    update

--- app/src/components/basis/footer/index.tsx
+++ app/src/components/basis/footer/index.tsx
@@ -7,7 +7,7 @@ const FooterSection: FC<{ heading: string; children?: ReactNode }> = ({
   heading,
 }) => {
   return (
-    <section className="flex-col flex flex-1 p-20 gap-4">
+    <section className="flex-col flex flex-1 gap-8">
       <h3 className="font-semibold text-xl">{heading}</h3>
       <div className="text-slate-300">{children}</div>
     </section>
@@ -17,7 +17,7 @@ const FooterSection: FC<{ heading: string; children?: ReactNode }> = ({
 export const Footer: FC<FooterProps> = ({}) => {
   return (
     <footer className="bg-footer-bg flex flex-col">
-      <section className="flex min-h-[384px] justify-center px-24">
+      <section className="flex min-h-[384px] justify-center px-36 py-20 gap-16">
         <FooterSection heading="About us">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et
           felis tellus. Duis accumsan turpis a diam auctor, vel suscipit leo
@@ -30,7 +30,12 @@ export const Footer: FC<FooterProps> = ({}) => {
         <FooterSection heading="Contact us"></FooterSection>
       </section>
       <section className="h-24">
-        <hr className="w-[70%] border-cyan-100 h-0 border-t mx-auto" />
+        <hr className="w-[70%] mx-auto border-0 bg-slate-400/50 h-[1px]" />
+        <div className="px-36 flex items-center justify-end h-full">
+          <span className="text-sm text-slate-300">
+            2023-{new Date().getFullYear()} &copy; Tim, Mykola & Elena
+          </span>
+        </div>
       </section>
     </footer>
   );
