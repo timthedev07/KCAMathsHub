4d928d139348f5cc9a53a1b8022aa0a4e7453488
timthedev07 <timpersonal07@gmail.com>
2023-11-25 16:27:50 +0100

    init not found

---
+++ app/src/app/not-found.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { NextPage } from "next";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+const NotFound: NextPage = () => {
+  const router = useRouter();
+  return (
+    <div className="w-full h-[90vh] flex flex-col gap-10 justify-center items-center pt-24 text-center">
+      <h1 className="text-red-400 font-bold text-8xl">404</h1>
+      <h4 className="text-red-200/80 text-lg font-semibold">Page not Found</h4>
+      <div className="w-full flex justify-center items-center gap-10">
+        <button
+          onClick={() => {
+            router.back();
+          }}
+        >
+          Previous
+        </button>
+        <Link href="/">Home</Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
