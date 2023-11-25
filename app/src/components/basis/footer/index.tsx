d2fec9958dbc704f1d576d048c1b33a36d7e8159
timthedev07 <timpersonal07@gmail.com>
2023-11-25 16:03:09 +0100

    complete footer lol

--- app/src/components/basis/footer/index.tsx
+++ app/src/components/basis/footer/index.tsx
@@ -1,53 +1,103 @@
 import { FC, ReactNode } from "react";
 import { LogoSVG } from "../../Logo";
+import Link from "next/link";
+import { InstagramSVG } from "../../../svgs/social/Instagram";
+import { XSVG } from "../../../svgs/social/X";
+import { EmailSVG } from "../../../svgs/social/Email";
 
 interface FooterProps {}
 
-const sBase = "flex-col flex flex-1 gap-8 lg:gap-0";
-const bt = "border-b border-slate-300/30 lg:border-0 pt-4 lg:pt-0 h-14";
+const sBase = "flex-col flex flex-1 gap-4 lg:gap-0 px-16 lg:px-0";
+
+const contactInfo = [
+  ["Tim", "tim.bao"],
+  ["Mykola", "mykola.sta"],
+  ["Elena", "elena.zhu"],
+];
 
 const FooterSection: FC<{ heading: string; children?: ReactNode }> = ({
   children,
   heading,
 }) => {
   return (
-    <section className={`${sBase} mt-10`}>
-      <h3 className={`${bt} font-semibold text-xl`}>{heading}</h3>
-      <div className={"text-slate-300"}>{children}</div>
+    <section className={`${sBase} mt-8`}>
+      <h3
+        className={`border-b border-slate-300/30 lg:border-0 pt-4 lg:pt-0 h-12 lg:h-auto lg:mb-2 font-semibold uppercase`}
+      >
+        {heading}
+      </h3>
+      <div className={"text-slate-300/80"}>{children}</div>
     </section>
   );
 };
 
 export const Footer: FC<FooterProps> = ({}) => {
+  const y = new Date().getFullYear();
+  const startY = 2023;
   return (
-    <footer className="bg-gradient-to-br from-footer-bg-a to-slate-950 from-0% to-80% flex flex-col">
-      <section className="flex min-h-[384px] justify-center px-16 lg:px-36 py-20 gap-16 flex-col lg:flex-row">
+    <footer className="bg-gradient-to-br from-footer-bg-a to-slate-950 from-0% to-80% flex flex-col lg:px-24">
+      <section className="flex lg:h-72 h-max justify-center py-20 gap-16 flex-col lg:flex-row">
         <section
-          className={`${sBase} w-full min-w-[25%] items-center justify-start`}
+          className={`${sBase} w-full h-32 lg:h-full flex-1 items-center lg:items-start justify-center`}
         >
-          <div className="flex w-[80%] lg:w-auto justify-center items-center gap-2 rounded-lg p-4 px-8 glass-morphism border-slate-300/20 border bg-opacity-30 bg-slate-900">
+          <Link
+            href="/"
+            className="transition-all duration-200 hover:scale-[1.02] cursor-pointer flex h-full justify-center items-center gap-2 rounded-xl w-80 glass-morphism border-slate-300/20 border bg-opacity-30 bg-slate-900"
+          >
             <LogoSVG className="w-16 h-16" />
             <h1 className="font-bold font-roboto text-2xl">KCAMathsHub</h1>
-          </div>
+          </Link>
         </section>
-        <div className="flex flex-col lg:flex-row gap-24">
-          {" "}
+        <div className="flex flex-col lg:flex-row flex-1 grow-1">
           <FooterSection heading="Creators">
-            <ul>
+            <ul className="flex flex-col gap-1">
               <li>Tim</li>
               <li>Mykola</li>
               <li>Elena</li>
             </ul>
           </FooterSection>
-          <FooterSection heading="Links"></FooterSection>
-          <FooterSection heading="Contact us"></FooterSection>
+          <FooterSection heading="Legal">
+            <ul className="flex flex-col gap-1">
+              <li>
+                <Link href="/terms-and-conditions">Terms & Conditions</Link>
+              </li>
+              <li>
+                <Link href="/privacy-policies">Privacy Policies</Link>
+              </li>
+            </ul>
+          </FooterSection>
+          <FooterSection heading="Contact us">
+            <ul>
+              {contactInfo.map(([name, email]) => {
+                return (
+                  <li key={name}>
+                    <Link
+                      className="flex items-center text-slate-300/80 group"
+                      href={`mailto:${email}@kcpupils.org`}
+                    >
+                      <EmailSVG className="w-5 h-5 mr-2 transition duration-200 group-hover:text-slate-100" />
+                      <span className="group-hover:text-slate-100">{name}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </FooterSection>
         </div>
       </section>
-      <section className="h-24">
-        <hr className="w-[70%] mx-auto border-0 bg-slate-400/50 h-[1px]" />
-        <div className="px-36 flex items-center justify-end h-full">
+      <section className="h-24 px-16 lg:px-0">
+        <hr className="w-full mx-auto border-0 bg-slate-400/50 h-[1px] hidden lg:block" />
+        <div className="flex items-center justify-between h-full ">
+          <div className="flex gap-4">
+            <Link href="" passHref>
+              <InstagramSVG className="w-8 text-secondary-accent-bg" />
+            </Link>
+            <Link href="" passHref>
+              <XSVG className="w-8 text-secondary-accent-bg" />
+            </Link>
+          </div>
           <span className="text-sm text-slate-300">
-            2023-{new Date().getFullYear()} &copy; Tim, Mykola & Elena
+            {startY === y ? y : `${startY}-${y}`} &copy; Tim, Mykola & Elena
           </span>
         </div>
       </section>
