3b5de5d034da1b160d09e8d1b1988323d7184c01
timthedev07 <timpersonal07@gmail.com>
2023-11-27 21:03:10 +0100

    ui fix lol

--- app/src/app/error/page.tsx
+++ app/src/app/error/page.tsx
@@ -10,7 +10,7 @@ const ErrorPage: NextPage = () => {
   const err = get("err") || "Unknown error...";
 
   return (
-    <div className="w-full flex justify-center items-center flex-col gap-8">
+    <div className="w-full h-[90vh] flex justify-center items-center flex-col gap-8">
       <h1 className="text-8xl text-red-400 font-bold">Blunder...</h1>
       <p className="text-red-400/80 font-medium">{err}</p>
       <div className="flex justify-between w-full max-w-[300px]">
