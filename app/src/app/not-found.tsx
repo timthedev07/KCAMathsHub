572daa84e2d69d67c090d0af9b209c8d2f08a312
timthedev07 <timpersonal07@gmail.com>
2023-11-28 22:09:41 +0100

    complete 404

--- app/src/app/not-found.tsx
+++ app/src/app/not-found.tsx
@@ -8,19 +8,20 @@ import { Button } from "flowbite-react";
 const NotFound: NextPage = () => {
   const router = useRouter();
   return (
-    <div className="w-full flex flex-col gap-10 justify-center items-center pt-24 text-center">
-      <h1 className="text-red-400 font-bold text-8xl">404</h1>
+    <div className="bg-gradient-to-br from-black to-[#130F40] to-50% w-full h-[90vh] flex flex-col gap-10 justify-center items-center pt-24 text-center">
+      <h1 className="text-red-400 font-bold text-8xl font-mono">404</h1>
       <h4 className="text-red-200/80 text-lg font-semibold">Page not Found</h4>
       <div className="w-full flex justify-center items-center gap-10">
-        <button
+        <Button
           onClick={() => {
             router.back();
           }}
+          gradientDuoTone="purpleToBlue"
         >
           Previous
-        </button>
+        </Button>
         <Link href="/" passHref>
-          <Button color="purple-blue">Home</Button>
+          <Button gradientDuoTone="greenToBlue">Home</Button>
         </Link>
       </div>
     </div>
