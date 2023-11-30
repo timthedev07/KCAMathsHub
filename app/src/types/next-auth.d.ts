c4c77152c51764b6865194f799b9d6b4fc7bc449
timthedev07 <timpersonal07@gmail.com>
2023-12-01 00:14:17 +0100

    fix type of role

--- app/src/types/next-auth.d.ts
+++ app/src/types/next-auth.d.ts
@@ -2,18 +2,14 @@ import { User as PUser } from "@prisma/client";
 import "next-auth";
 import { Role } from "./role";
 
+type WithRole = PUser & { roles: Role[] };
+
 declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user: PUser;
+    user: WithRole;
   }
-  interface User extends PUser {}
-}
-
-declare module "next-auth/jwt" {
-  interface JWT {
-    role: Role;
-  }
+  interface User extends WithRole {}
 }
