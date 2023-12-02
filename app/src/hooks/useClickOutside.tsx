f7589381f505cdf678a3977763641eb0cb20df2a
timthedev07 <timpersonal07@gmail.com>
2023-12-03 00:22:17 +0100

    add hook

---
+++ app/src/hooks/useClickOutside.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, MutableRefObject, MouseEvent } from "react";
+
+export const useClickOutside = <T extends HTMLElement>(
+  ref: MutableRefObject<T | null>,
+  action: Function
+) => {
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent<T, MouseEvent>) {
+      if (ref.current && !ref.current.contains(event.target as any)) {
+        action();
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside as any);
+
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside as any);
+    };
+  }, [ref]);
+};
