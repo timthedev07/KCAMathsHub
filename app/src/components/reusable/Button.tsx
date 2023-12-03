4f6018dc0d6d471122a743d7b8d21700024c084a
timthedev07 <timpersonal07@gmail.com>
2023-12-03 23:16:00 +0100

    fuck flowbite buttons

--- app/src/components/reusable/Button.tsx
+++ app/src/components/reusable/Button.tsx
@@ -6,11 +6,11 @@ export const Button: FC<Parameters<typeof FBButton>[0]> = (props) => {
     <FBButton
       theme={{
         size: {
-          xs: "text-xs px-2 py-1",
-          sm: "text-sm px-3 py-1.5",
-          md: "text-sm px-4 py-2",
-          lg: "text-base px-5 py-2.5",
-          xl: "text-base px-6 py-3",
+          xs: "text-xs px-2 py-0.5",
+          sm: "text-sm px-3 py-1",
+          md: "text-sm px-4 py-1.5",
+          lg: "text-base px-5 py-2",
+          xl: "text-base px-6 py-2.5",
         },
       }}
       {...props}
