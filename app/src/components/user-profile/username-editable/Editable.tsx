103a8730b280627f3923ce5dbd38c5c0cb2e256d
timthedev07 <timpersonal07@gmail.com>
2023-12-04 08:34:20 +0100

    add await

--- app/src/components/user-profile/username-editable/Editable.tsx
+++ app/src/components/user-profile/username-editable/Editable.tsx
@@ -67,7 +67,7 @@ export const Editable: FC<EditableProps> = ({ user }) => {
             setEditable(false);
             setSuccess(true);
             setShowToast(true);
-            update({ user: { ...user, username: inputUsername } });
+            await update({ user: { ...user, username: inputUsername } });
           } catch (err) {
             const msg = (err as any).message;
             setSuccess(false);
