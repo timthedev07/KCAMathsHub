be10e53a542aebfbae6e42dcf6943ebb9c5edda1
timthedev07 <timpersonal07@gmail.com>
2023-12-04 10:56:24 +0100

    fix shit

--- app/src/lib/withClientSession.tsx
+++ app/src/lib/withClientSession.tsx
@@ -2,11 +2,9 @@ import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { FC } from "react";
 
-export const withClientSession = <T extends { session: Session | null }>(
-  Component: FC<T>
-): FC<T> => {
+export const withClientSession = <T,>(Component: FC<T>) => {
   // eslint-disable-next-line react/display-name
-  return (props: T) => (
+  return (props: T & { session: Session | null }) => (
     <SessionProvider session={props.session}>
       {<Component {...props} />}
     </SessionProvider>
