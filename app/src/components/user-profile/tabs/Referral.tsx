a2b2aaf90e1f316613b99357cca23e0d022d7e60
timthedev07 <timpersonal07@gmail.com>
2023-12-04 12:34:44 +0100

    referral tab init

--- app/src/components/user-profile/tabs/Referral.tsx
+++ app/src/components/user-profile/tabs/Referral.tsx
@@ -1,8 +1,22 @@
+"use client";
 import { TabItem } from "flowbite-react";
+import { useSession } from "next-auth/react";
 import { FC } from "react";
+import { InitReferral } from "../../InitReferral";
+import { Session } from "next-auth";
 
-interface ReferralTabProps {}
+interface ReferralTabProps {
+  session: Session | null;
+}
 
 export const ReferralTab: FC<ReferralTabProps> = ({}) => {
-  return <TabItem title="Referral"></TabItem>;
+  const { data } = useSession();
+
+  return (
+    <TabItem title="Referral">
+      <div>
+        <InitReferral userId={data?.user.id || ""} />
+      </div>
+    </TabItem>
+  );
 };
