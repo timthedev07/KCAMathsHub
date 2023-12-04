a18aef482667254c941c2e7d87f40afd2215e3ab
timthedev07 <timpersonal07@gmail.com>
2023-12-04 10:31:58 +0100

    complete user expandable

--- app/src/components/basis/user-expandable/index.tsx
+++ app/src/components/basis/user-expandable/index.tsx
@@ -2,6 +2,9 @@
 import { Avatar } from "flowbite-react";
 import { Session } from "next-auth";
 import { Dispatch, FC, SetStateAction } from "react";
+import { BadgesDisplay } from "../../user-profile/BadgesDisplay";
+import { signOut } from "next-auth/react";
+import { Button } from "../../reusable/Button";
 
 interface UserExpandableProps {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
@@ -23,33 +26,25 @@ export const UserExpandable: FC<UserExpandableProps> = ({
         }}
         className="absolute left-0 top-0 w-[2400px] h-[10000px]"
       ></div>
-      <div className="fixed right-[4%] top-20 w-[250px] max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 pt-5">
-        <div className="flex flex-col items-center pb-10">
-          <Avatar
-            img={user.image || undefined}
-            size="30px"
-            className="mb-5"
-            rounded
-          />
-          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-            {user.username.length > 20 ? "Star Kolya" : user.username}
+      <div className="fixed right-4 top-20 max-w-[368px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 pt-10 px-8">
+        <div className="flex flex-col gap-2 items-center pb-10">
+          <Avatar img={user.image || undefined} size="30px" rounded />
+          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white font-mono">
+            {user.username}
           </h5>
-          <span className="text-sm text-gray-500 dark:text-gray-400">
-            {user.roles}
-          </span>
-          <div className="flex mt-4 md:mt-6">
-            <button
-              type="button"
-              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ml-3"
-            >
+          <BadgesDisplay center roles={user.roles} />
+          <div className="flex mt-4 md:mt-6 gap-4">
+            <Button color="lime">
               <a href="/user/profile">Profile</a>
-            </button>
-            <button
-              type="button"
-              className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            </Button>
+            <Button
+              onClick={async () => {
+                await signOut();
+              }}
+              color="teal"
             >
               Log out
-            </button>
+            </Button>
           </div>
         </div>
       </div>
