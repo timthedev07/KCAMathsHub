45593aadd3c5e6f3602e040b4d874e8cf446c591
timthedev07 <timpersonal07@gmail.com>
2023-12-05 23:41:38 +0100

    fix trpc setup

--- app/src/trpc/Provider.tsx
+++ app/src/trpc/Provider.tsx
@@ -1,18 +1,40 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
+import { getFetch, httpBatchLink, loggerLink } from "@trpc/client";
 import React, { useState } from "react";
 import { trpc } from "./client";
+import superjson from "superjson";
 
 export const TRPCProvider = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = useState(() => new QueryClient({}));
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { staleTime: 5000 } },
+      })
+  );
+
+  const url = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000/api/trpc/";
+
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
+        loggerLink({
+          enabled: () => true,
+        }),
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url,
+          fetch: async (input, init?) => {
+            const fetch = getFetch();
+            return fetch(input, {
+              ...init,
+              credentials: "include",
+            });
+          },
         }),
       ],
+      transformer: superjson,
     })
   );
   return (
