c6e3198c33b9f7dbe6626c722e4874634091f2bb
timthedev07 <timpersonal07@gmail.com>
2023-12-06 15:10:16 +0100

    complete qlist component

---
+++ app/src/components/user-profile/tabs/main/item-display/QList.tsx
@@ -0,0 +1,42 @@
+import { FC } from "react";
+import { ProfileSubDisplay } from "../ProfileSubDisplay";
+import { Question } from "./Question";
+import { trpc } from "../../../../../trpc/client";
+import { LoadingSpin } from "../../../../LoadingSpin";
+
+interface QListProps {
+  uid: string;
+}
+
+export const QList: FC<QListProps> = ({ uid }) => {
+  const { data, status } = trpc.getUserDisplayQuestions.useQuery({ uid });
+
+  return (
+    <ProfileSubDisplay>
+      {status === "loading" || !data ? (
+        <LoadingSpin size="lg" />
+      ) : (
+        <div className="flex flex-col gap-3 pt-4">
+          <h2 className="w-full text-center font-bold text-3xl my-8">
+            Questions
+          </h2>
+          <ol className="last:border-b-0 flex flex-col">
+            {[...data, ...data, ...data, ...data, ...data, ...data].map(
+              (each, ind) => (
+                <>
+                  <Question question={each} key={each.id} />
+                  {ind <
+                  [...data, ...data, ...data, ...data, ...data, ...data]
+                    .length -
+                    1 ? (
+                    <hr className="h-[1px] border-0 bg-slate-400/30" />
+                  ) : null}
+                </>
+              )
+            )}
+          </ol>
+        </div>
+      )}
+    </ProfileSubDisplay>
+  );
+};
