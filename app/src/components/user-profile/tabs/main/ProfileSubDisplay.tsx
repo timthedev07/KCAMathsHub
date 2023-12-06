38c9046018cb84497b9b1a8acbf6e3ad1a03b0ca
timthedev07 <timpersonal07@gmail.com>
2023-12-06 15:10:23 +0100

    fix display

--- app/src/components/user-profile/tabs/main/ProfileSubDisplay.tsx
+++ app/src/components/user-profile/tabs/main/ProfileSubDisplay.tsx
@@ -4,7 +4,7 @@ import { viewPanelBase } from "..";
 export const ProfileSubDisplay: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div
-      className={`${viewPanelBase} overflow-hidden overflow-y-scroll lg:flex-1 w-full h-96 lg:h-1/2`}
+      className={`${viewPanelBase} overflow-hidden overflow-y-scroll lg:flex-1 w-full h-[486px] lg:h-1/2`}
     >
       {children}
     </div>
