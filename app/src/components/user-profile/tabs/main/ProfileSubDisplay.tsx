dd4fae30f848b79e8797cabdc54aa72814773eeb
timthedev07 <timpersonal07@gmail.com>
2023-12-06 12:59:09 +0100

    enforce rounded corners on profile subdisplay

--- app/src/components/user-profile/tabs/main/ProfileSubDisplay.tsx
+++ app/src/components/user-profile/tabs/main/ProfileSubDisplay.tsx
@@ -2,5 +2,11 @@ import { FC, PropsWithChildren } from "react";
 import { viewPanelBase } from "..";
 
 export const ProfileSubDisplay: FC<PropsWithChildren> = ({ children }) => {
-  return <div className={`${viewPanelBase} flex-1 h-1/2`}>{children}</div>;
+  return (
+    <div
+      className={`${viewPanelBase} overflow-hidden overflow-y-scroll lg:flex-1 w-full h-96 lg:h-1/2`}
+    >
+      {children}
+    </div>
+  );
 };
