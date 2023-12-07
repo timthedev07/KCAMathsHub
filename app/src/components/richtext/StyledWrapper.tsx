c8cec1fa31ee9202d82d327e82f7cbcf37a2f5f0
timthedev07 <timpersonal07@gmail.com>
2023-12-07 20:50:53 +0100

    add styled wrapper for editor

---
+++ app/src/components/richtext/StyledWrapper.tsx
@@ -0,0 +1,14 @@
+import { FC, PropsWithChildren } from "react";
+
+export const StyledWrapper: FC<PropsWithChildren<{ className?: string }>> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div
+      className={`border border-slate-400/40 rounded-lg p-4 bg-slate-300/[0.07] ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
