e6402277e0eee9494f772f5f656cff5f4a5fcb52
timthedev07 <timpersonal07@gmail.com>
2023-12-07 16:54:46 +0100

    fix

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -6,7 +6,7 @@ const Home = async () => {
   const uid = session?.user.id;
 
   return (
-    <div className="p-8 sm:p-24 md:p-64 flex flex-col gap-12">
+    <div className="p-8  flex flex-col gap-12">
       {!!uid ? (
         <>
           <QuestionForm userId={uid} />
