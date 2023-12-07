36cb6ea77408d094e9793adc11883cb35adbaac6
timthedev07 <timpersonal07@gmail.com>
2023-12-08 00:40:13 +0100

    add profile image display component

---
+++ app/src/components/ProfileImgDisplay.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import { ProfileImgPlaceholderSVG } from "../svgs/ProfileImgPlaceholder";
+import Image from "next/image";
+
+interface ProfileImgDisplayProps {
+  src: string | null | undefined;
+  className?: string;
+}
+
+export const ProfileImgDisplay: FC<ProfileImgDisplayProps> = ({
+  src,
+  className,
+}) => {
+  return (
+    <div
+      className={`${
+        !className ? "w-10 h-10" : className
+      } relative rounded text-neutral-400 select-none`}
+    >
+      {src ? (
+        <Image
+          src={src}
+          fill
+          alt="user profile image"
+          className="rounded-full"
+        />
+      ) : (
+        <ProfileImgPlaceholderSVG className="w-full h-full" />
+      )}
+    </div>
+  );
+};
