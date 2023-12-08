f8927de8153e2123e26ec01d7c99dac1a65ae6db
timthedev07 <timpersonal07@gmail.com>
2023-12-08 01:45:16 +0100

    complete initial question asking page
    the design depends further on how answers are formatted
    
    consequently we will have to build the answer composing page first

--- app/src/app/questions/[quid]/page.tsx
+++ app/src/app/questions/[quid]/page.tsx
@@ -1,17 +1,31 @@
 import Image from "next/image";
+import { MDXRemote } from "next-mdx-remote/rsc";
 import { redirect } from "next/navigation";
-import { getServerSession } from "../../../lib/authoptions";
 import { pageURLs } from "../../../lib/pageURLGen";
 import { SSRCaller } from "../../../server";
 import { NextPage } from "../../../types/nextpage";
+import { getServerSession } from "../../../lib/authoptions";
+import remarkGfm from "remark-gfm";
+import { ProfileImgDisplay } from "../../../components/ProfileImgDisplay";
+import { FC, PropsWithChildren } from "react";
+import Link from "next/link";
+import { dateTimeDisplay } from "../../../lib/datetimeDisplay";
+import { Button } from "../../../components/reusable/Button";
+import { FaCheckCircle } from "react-icons/fa";
 
 interface Props {
   params: { quid: string };
 }
 
+const OptionalLinkWrapper: FC<
+  PropsWithChildren<{ hasLink: boolean; href: string }>
+> = ({ children, hasLink, href }) => {
+  return hasLink ? <Link href={href}>{children}</Link> : children;
+};
+
 const getSSRProps = async (quid: string) => {
-  const session = await getServerSession();
   const question = await SSRCaller.getQuestion({ quid });
+  const session = await getServerSession();
 
   if (!question) {
     redirect(pageURLs.error("Question not found..."));
@@ -20,16 +34,85 @@ const getSSRProps = async (quid: string) => {
   return { u: session?.user, question };
 };
 
+const ap = "px-5";
+
 const Question: NextPage<Props> = async ({ params: { quid } }) => {
-  const { question } = await getSSRProps(quid);
+  const {
+    question: {
+      questioner,
+      title,
+      content,
+      attachments,
+      anonymous,
+      timestamp,
+      answered,
+    },
+    u,
+  } = await getSSRProps(quid);
 
   return (
-    <div>
-      <div className="dev-border-cyan p-12 w-full flex-col flex gap-8">
-        <h1 className="font-semibold text-5xl break-words">{question.title}</h1>
-        <p className="break-words w-full">{question.content}</p>
+    <div className="flex flex-col items-center">
+      <div className="min-w-[300px] max-w-[700px] w-full flex flex-col gap-12 py-24 md:px-0 px-12">
+        <div className="flex flex-col gap-4">
+          <h1
+            className={`font-bold text-4xl break-words w-full flex justify-between items-center ${
+              answered ? "bg-green-300/20 py-4 rounded-xl " + ap : ""
+            }`}
+          >
+            <span>{title}</span>
+            {answered ? (
+              <FaCheckCircle className="text-green-400 w-6 h-6" />
+            ) : null}
+          </h1>
+          <span className={`text-sm text-white/60 ${answered ? ap : ""}`}>
+            {dateTimeDisplay(timestamp)}
+          </span>
+          <div
+            className={`mt-5 flex justify-between items-center ${
+              answered ? ap : ""
+            }`}
+          >
+            <OptionalLinkWrapper
+              hasLink={!anonymous}
+              href={pageURLs.user(questioner?.id || "")}
+            >
+              <div className="flex gap-3 items-center w-fit">
+                <ProfileImgDisplay
+                  className="w-8 h-8"
+                  src={questioner?.image}
+                />
+                <span className={`text-white/80 ${anonymous ? "italic" : ""}`}>
+                  {questioner?.username || "Anonymous"}
+                </span>
+              </div>
+            </OptionalLinkWrapper>
+            {u &&
+            (u.roles.includes("answerer") ||
+              u.id === (questioner?.id || "")) ? (
+              <Link href={pageURLs.answerQuestion(quid)}>
+                <Button color={"green"}>Answer</Button>
+              </Link>
+            ) : null}
+          </div>
+        </div>
+        <hr className="h-[1px] border-0 bg-slate-300/30 w-full" />
+        {/* <StyledWrapper> */}
+        <div className="overflow-x-scroll min-h-[200px]">
+          <MDXRemote
+            source={content}
+            options={{
+              mdxOptions: {
+                remarkPlugins: [remarkGfm],
+              },
+            }}
+          />
+        </div>
+        {/* </StyledWrapper> */}
+
+        <h2 className="font-semibold text-3xl">Attachments</h2>
+
         <ul className="flex dev-border-green gap-8 p-4 h-96">
-          {question.attachments.map((each) => (
+          {attachments.map((each) => (
             <li
               key={each.name + each.imgUrl}
               className="h-full w-96 dev-border-red relative"
