d94f7c73fb7079e35a8d090235ade93677c65484
timthedev07 <timpersonal07@gmail.com>
2023-12-08 16:30:23 +0100

    complete table styling

--- app/src/components/mdx/components.tsx
+++ app/src/components/mdx/components.tsx
@@ -1,11 +1,32 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { ComponentProps } from "react";
 
+const tableShared = "py-2 px-4 border border-slate-400/40";
+
 export const mdxCustomComponents: ComponentProps<
   typeof MDXRemote
 >["components"] = {
-  table: () => {
-    return <></>;
+  table: ({ className: _, ...props }) => {
+    return (
+      <div className="rounded-lg overflow-hidden mx-auto border border-slate-400/40 min-w-[250px] max-w-[600px] w-11/12 md:w-10/12 lg:9/12 xl:8-12">
+        <table
+          className="border-hidden border-collapse w-full h-full border-spacing-0"
+          {...props}
+        ></table>
+      </div>
+    );
+  },
+  // tbody: ({ className: _, ...props }) => {
+  //   return <tbody className="border-0" {...props}></tbody>;
+  // },
+  td: ({ className: _, ...props }) => {
+    return <td className={`${tableShared}`} {...props}></td>;
+  },
+  tr: ({ ...props }) => {
+    return <tr {...props}></tr>;
+  },
+  th: ({ className: _, ...props }) => {
+    return <th className={`${tableShared} bg-emerald-700/90`} {...props}></th>;
   },
   h1: () => {
     return <></>;
@@ -53,15 +74,6 @@ export const mdxCustomComponents: ComponentProps<
     return <></>;
   },
   blockquote: () => {
-    return <></>;
-  },
-  td: () => {
-    return <></>;
-  },
-  tr: () => {
-    return <></>;
-  },
-  th: () => {
     return <></>;
   },
 };
