e48f6f0f26bcc27988f8e09126bb73dab7cab82a
timthedev07 <timpersonal07@gmail.com>
2023-12-08 22:54:04 +0100

    semi-complete attachment list

--- app/src/components/attachments/index.tsx
+++ app/src/components/attachments/index.tsx
@@ -1,22 +1,17 @@
 import Image from "next/image";
 import { FC } from "react";
+import { Button } from "../reusable/Button";
 
 interface AttachmentListProps {
-  attachments: { name: string; imgUrl: string }[];
+  attachments: { name: string; imgUrl: string; size: number }[];
 }
 
 export const AttachmentList: FC<AttachmentListProps> = ({ attachments }) => {
   return (
-    <ol className="w-full flex flex-col">
-      {attachments.map((each, ind) => (
+    <ol className="w-full flex flex-col gap-4">
+      {attachments.map((each) => (
         <li
-          className={`flex gap-3 py-6 px-8 cursor-pointer transition duration-200 hover:bg-slate-400/[0.12] bg-slate-400/10 ${
-            ind === 0
-              ? "rounded-t-lg"
-              : ind === attachments.length - 1
-              ? "rounded-b-lg"
-              : ""
-          }`}
+          className={`flex items-center gap-3 py-5 shadow-2xl px-8 cursor-pointer transition duration-200 hover:bg-slate-400/[0.12] bg-slate-500/10 rounded-lg`}
           key={each.name + "_" + each.imgUrl}
         >
           <div className="w-14 h-14 relative">
@@ -26,7 +21,15 @@ export const AttachmentList: FC<AttachmentListProps> = ({ attachments }) => {
               fill
               className="rounded-md object-cover object-center"
             />
-            <span>{each.name}</span>
+          </div>
+          <div className="flex flex-col justify-evenly h-full">
+            <span className="font-semibold text-white/80 text-sm">
+              {each.name}
+            </span>
+            <span className="text-xs text-white/60">{each.size}MB</span>
+          </div>
+          <div className="ml-auto">
+            <Button>View File</Button>
           </div>
         </li>
       ))}
