f4682ffe0f3ffcbd471f4872f5c034e90efbf369
timthedev07 <timpersonal07@gmail.com>
2023-12-08 17:40:55 +0100

    complete md components

--- app/src/components/mdx/components.tsx
+++ app/src/components/mdx/components.tsx
@@ -1,5 +1,7 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
+import Link from "next/link";
 import { ComponentProps } from "react";
+import { pageURLs } from "../../lib/pageURLGen";
 
 const tableShared = "py-2 px-4 border border-slate-400/40";
 
@@ -16,9 +18,6 @@ export const mdxCustomComponents: ComponentProps<
       </div>
     );
   },
-  // tbody: ({ className: _, ...props }) => {
-  //   return <tbody className="border-0" {...props}></tbody>;
-  // },
   td: ({ className: _, ...props }) => {
     return <td className={`${tableShared}`} {...props}></td>;
   },
@@ -28,52 +27,81 @@ export const mdxCustomComponents: ComponentProps<
   th: ({ className: _, ...props }) => {
     return <th className={`${tableShared} bg-emerald-700/90`} {...props}></th>;
   },
-  h1: () => {
-    return <></>;
+  h1: ({ className: _, ...props }) => {
+    return <h1 className="text-2xl font-bold text-white" {...props}></h1>;
   },
-  h2: () => {
-    return <></>;
+  h2: ({ className: _, ...props }) => {
+    return <h2 className="text-xl font-semibold text-white/90" {...props}></h2>;
   },
-  h3: () => {
-    return <></>;
+  h3: ({ className: _, ...props }) => {
+    return (
+      <h3
+        className="text-lg font-semibold text-white/70 underline"
+        {...props}
+      ></h3>
+    );
   },
-  h4: () => {
-    return <></>;
+  h4: ({ className: _, ...props }) => {
+    return (
+      <h4
+        className="text-base font-medium text-white/70 underline"
+        {...props}
+      ></h4>
+    );
   },
-  h5: () => {
-    return <></>;
+  h5: ({ className: _, ...props }) => {
+    return (
+      <h5
+        className="text-base font-medium text-white/70 underline"
+        {...props}
+      ></h5>
+    );
   },
-  h6: () => {
-    return <></>;
+  h6: ({ className: _, ...props }) => {
+    return (
+      <h6
+        className="text-base font-medium text-white/70 underline"
+        {...props}
+      ></h6>
+    );
   },
-  hr: () => {
-    return <></>;
+  hr: ({ className: _, ...props }) => {
+    return <hr className="h-[1px] border-0 bg-slate-400/20 my-8" {...props} />;
   },
-  a: () => {
-    return <></>;
+  a: ({ ref: _, href, className: __, ...props }) => {
+    return (
+      <Link
+        className="text-cyan-300/80 hover:text-cyan-300 transition duration-100"
+        href={href || pageURLs.notFound()}
+        {...props}
+      ></Link>
+    );
   },
-  em: () => {
-    return <></>;
+  em: ({ className: _, ...props }) => {
+    return <em {...props}></em>;
   },
-  i: () => {
-    return <></>;
+  i: ({ className: _, ...props }) => {
+    return <i {...props}></i>;
   },
-  strong: () => {
-    return <></>;
+  strong: ({ className: _, ...props }) => {
+    return <strong className="text-white" {...props}></strong>;
   },
-  ul: () => {
-    return <></>;
+  ul: ({ className: _, ...props }) => {
+    return <ul className="list-disc list-inside pl-[3px]" {...props}></ul>;
   },
-  ol: () => {
-    return <></>;
+  ol: ({ className: _, ...props }) => {
+    return <ol className="list-decimal list-inside" {...props}></ol>;
   },
-  li: () => {
-    return <></>;
+  li: ({ className: _, ...props }) => {
+    return <li {...props} className="pl-3 text-white/80"></li>;
   },
-  u: () => {
-    return <></>;
+  u: ({ className: _, ...props }) => {
+    return <u {...props}></u>;
   },
   blockquote: () => {
     return <></>;
+  },
+  p: ({ ...props }) => {
+    return <p {...props} className="my-2 text-white/70"></p>;
   },
 };
