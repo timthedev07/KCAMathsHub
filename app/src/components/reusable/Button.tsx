054a7ad902f8793cd5af35d4f2b4efe14e3569ea
timthedev07 <timpersonal07@gmail.com>
2023-12-09 16:10:43 +0100

    tweak ugly button sizing

--- app/src/components/reusable/Button.tsx
+++ app/src/components/reusable/Button.tsx
@@ -7,7 +7,7 @@ export const Button: FC<ComponentProps<typeof FBButton>> = (props) => {
       theme={{
         size: {
           xs: "text-xs px-2 py-0.5",
-          sm: "text-sm px-3 py-1",
+          sm: "text-xs px-3 py-1",
           md: "text-sm px-4 py-1.5",
           lg: "text-base px-5 py-2",
           xl: "text-base px-6 py-2.5",
