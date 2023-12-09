c8d272ed98de977676184ca25757db991d4a34a8
timthedev07 <timpersonal07@gmail.com>
2023-12-09 16:24:41 +0100

    update question page with deletion button

--- app/src/app/questions/[quid]/page.tsx
+++ app/src/app/questions/[quid]/page.tsx
@@ -1,18 +1,19 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
+import Link from "next/link";
 import { redirect } from "next/navigation";
+import { FC, PropsWithChildren } from "react";
+import { FaCheckCircle } from "react-icons/fa";
+import remarkGfm from "remark-gfm";
+import { ProfileImgDisplay } from "../../../components/ProfileImgDisplay";
+import { AttachmentList } from "../../../components/attachments";
+import { mdxCustomComponents } from "../../../components/mdx/components";
+import { Button } from "../../../components/reusable/Button";
+import { getServerSession } from "../../../lib/authoptions";
+import { dateTimeDisplay } from "../../../lib/datetimeDisplay";
 import { pageURLs } from "../../../lib/pageURLGen";
 import { SSRCaller } from "../../../server";
 import { NextPage } from "../../../types/nextpage";
-import { getServerSession } from "../../../lib/authoptions";
-import remarkGfm from "remark-gfm";
-import { ProfileImgDisplay } from "../../../components/ProfileImgDisplay";
-import { FC, PropsWithChildren } from "react";
-import Link from "next/link";
-import { dateTimeDisplay } from "../../../lib/datetimeDisplay";
-import { Button } from "../../../components/reusable/Button";
-import { FaCheckCircle } from "react-icons/fa";
-import { mdxCustomComponents } from "../../../components/mdx/components";
-import { AttachmentList } from "../../../components/attachments";
+import { DeletionButtonWithConfirmation } from "./DeletionButtonWithConfirmation";
 
 interface Props {
   params: { quid: string };
@@ -38,13 +39,16 @@ const getSSRProps = async (quid: string) => {
     redirect(pageURLs.error("Question not found..."));
   }
 
-  return { u: session?.user, question };
+  const isOwner = session?.user.id === question.questionerId;
+
+  return { u: session?.user, question, isOwner };
 };
 
 const ap = "px-5";
 
 const Question: NextPage<Props> = async ({ params: { quid } }) => {
   const {
+    isOwner,
     question: {
       questioner,
       title,
@@ -53,73 +57,84 @@ const Question: NextPage<Props> = async ({ params: { quid } }) => {
       anonymous,
       timestamp,
       answered,
-      questionerId,
     },
     u,
   } = await getSSRProps(quid);
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="min-w-[300px] max-w-[700px] w-full flex flex-col gap-12 py-24 md:px-0 px-12">
-        <div className="flex flex-col gap-4">
-          <h1
-            className={`font-bold text-4xl break-words w-full flex justify-between items-center ${
-              answered ? "bg-green-300/20 py-4 rounded-xl " + ap : ""
-            }`}
-          >
-            <span>{title}</span>
-            {answered ? (
-              <FaCheckCircle className="text-green-400 w-6 h-6" />
-            ) : null}
-          </h1>
-          <span className={`text-sm text-white/60 ${answered ? ap : ""}`}>
-            {dateTimeDisplay(timestamp)}
-          </span>
-          <div
-            className={`mt-5 flex justify-between items-center ${
-              answered ? ap : ""
-            }`}
-          >
-            <OptionalLinkWrapper
-              hasLink={!anonymous}
-              href={pageURLs.user(questioner?.id || "")}
+    <>
+      <div className="flex flex-col items-center">
+        <div className="min-w-[300px] max-w-[700px] w-full flex flex-col gap-12 py-24 md:px-0 px-12">
+          <div className="flex flex-col gap-4">
+            <h1
+              className={`font-bold text-4xl break-words w-full flex justify-between items-center ${
+                answered ? "bg-green-300/20 py-4 rounded-xl " + ap : ""
+              }`}
             >
-              <div className="flex gap-3 items-center w-fit">
-                <ProfileImgDisplay
-                  className="w-8 h-8"
-                  src={questioner?.image}
+              <span>{title}</span>
+              {answered ? (
+                <FaCheckCircle className="text-green-400 w-6 h-6" />
+              ) : null}
+            </h1>
+            <span className={`text-sm text-white/60 ${answered ? ap : ""}`}>
+              {dateTimeDisplay(timestamp)}
+            </span>
+            <div
+              className={`mt-5 flex justify-between items-center ${
+                answered ? ap : ""
+              }`}
+            >
+              <OptionalLinkWrapper
+                hasLink={!anonymous}
+                href={pageURLs.user(questioner?.id || "")}
+              >
+                <div className="flex gap-3 items-center w-fit">
+                  <ProfileImgDisplay
+                    className="w-8 h-8"
+                    src={questioner?.image}
+                  />
+                  <span
+                    className={`text-white/80 ${anonymous ? "italic" : ""}`}
+                  >
+                    {questioner?.username || "Anonymous"}
+                  </span>
+                </div>
+              </OptionalLinkWrapper>
+            </div>
+            <div className="flex gap-3 ml-auto">
+              {u && (u.roles.includes("answerer") || isOwner) ? (
+                <Link prefetch={false} href={pageURLs.answerQuestion(quid)}>
+                  <Button color={"dark"}>Answer</Button>
+                </Link>
+              ) : null}
+              {u && isOwner ? (
+                <DeletionButtonWithConfirmation
+                  isOwner={isOwner}
+                  quid={quid}
+                  uid={u.id}
                 />
-                <span className={`text-white/80 ${anonymous ? "italic" : ""}`}>
-                  {questioner?.username || "Anonymous"}
-                </span>
-              </div>
-            </OptionalLinkWrapper>
-            {u &&
-            (u.roles.includes("answerer") || u.id === (questionerId || "")) ? (
-              <Link prefetch={false} href={pageURLs.answerQuestion(quid)}>
-                <Button color={"green"}>Answer</Button>
-              </Link>
-            ) : null}
+              ) : null}
+            </div>
           </div>
-        </div>
-        <hr className="h-[1px] border-0 bg-slate-300/30 w-full" />
-        <div className="overflow-x-scroll min-h-[200px]">
-          <MDXRemote
-            source={content}
-            options={{
-              mdxOptions: {
-                remarkPlugins: [remarkGfm],
-              },
-            }}
-            components={mdxCustomComponents}
-          />
-        </div>
+          <hr className="h-[1px] border-0 bg-slate-300/30 w-full" />
+          <div className="overflow-x-scroll min-h-[200px]">
+            <MDXRemote
+              source={content}
+              options={{
+                mdxOptions: {
+                  remarkPlugins: [remarkGfm],
+                },
+              }}
+              components={mdxCustomComponents}
+            />
+          </div>
 
-        <h2 className="font-semibold text-3xl">Attachments</h2>
+          <h2 className="font-semibold text-3xl">Attachments</h2>
 
-        <AttachmentList attachments={attachments} />
+          <AttachmentList attachments={attachments} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
