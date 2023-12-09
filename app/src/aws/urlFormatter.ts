62c4d69c92829f9e7feb47380bed63b2f7c9785b
timthedev07 <timpersonal07@gmail.com>
2023-12-09 01:31:35 +0100

    image attachment signing fn

--- app/src/aws/urlFormatter.ts
+++ app/src/aws/urlFormatter.ts
@@ -1,5 +1,12 @@
+import { getSignedUrl } from "@aws-sdk/cloudfront-signer";
+
 export const getUrl = (fname: string) => {
-  return `https://${process.env.S3_CLOUDFRONT_DOMAIN}/${encodeURIComponent(
-    fname
-  )}`;
+  return getSignedUrl({
+    url: `https://${process.env.S3_CLOUDFRONT_DOMAIN}/${encodeURIComponent(
+      fname
+    )}`,
+    privateKey: process.env.CLOUDFRONT_PRIVATE_KEY,
+    keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID,
+    dateLessThan: new Date(Date.now() + 1000 * 3600 * 24).toDateString(),
+  });
 };
