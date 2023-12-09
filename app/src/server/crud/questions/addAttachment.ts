08935777f7c3566344f7018fdd1bd9e6e41c4f54
timthedev07 <timpersonal07@gmail.com>
2023-12-09 15:08:48 +0100

    update mutation to take in obj key

--- app/src/server/crud/questions/addAttachment.ts
+++ app/src/server/crud/questions/addAttachment.ts
@@ -8,7 +8,7 @@ export const addAttachments = publicProcedure
     z.array(
       z.object({
         attachmentName: z.string(),
-        url: z.string(),
+        objKey: z.string(),
         size: z.number().nonnegative(),
         answerId: z.string().optional(),
         questionId: z.string().optional(),
@@ -18,7 +18,7 @@ export const addAttachments = publicProcedure
   .mutation(async ({ input: attachments }) => {
     const ids = [];
     for (const {
-      url,
+      objKey,
       attachmentName,
       answerId,
       questionId,
@@ -27,13 +27,13 @@ export const addAttachments = publicProcedure
       if (!!answerId && !!questionId) {
         throw new TRPCError({
           code: "BAD_REQUEST",
-          message: `Only one of 'answerId' and 'questionId' of '${url} (${attachmentName})' should be provided to indicate the specified relation.`,
+          message: `Only one of 'answerId' and 'questionId' of '${objKey} (${attachmentName})' should be provided to indicate the specified relation.`,
         });
       }
       const { id } = await prisma.imageAttachment.create({
         data: {
           size,
-          imgUrl: url,
+          objKey,
           name: attachmentName,
           answerId,
           questionId,
