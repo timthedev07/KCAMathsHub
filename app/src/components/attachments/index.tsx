ccd7e92c62b0712823104fca14047b85dc894e1b
timthedev07 <timpersonal07@gmail.com>
2023-12-09 20:20:04 +0100

    destructure for easier access

--- app/src/components/attachments/index.tsx
+++ app/src/components/attachments/index.tsx
@@ -9,24 +9,24 @@ interface AttachmentListProps {
 export const AttachmentList: FC<AttachmentListProps> = ({ attachments }) => {
   return (
     <ol className="w-full flex flex-col gap-4">
-      {attachments.map((each) => (
+      {attachments.map(({ url, name, size }) => (
         <li
           className={`flex items-center gap-3 py-5 shadow-2xl px-8 cursor-pointer transition duration-200 hover:bg-slate-400/[0.12] bg-slate-500/10 rounded-lg`}
-          key={each.name + "_" + each.url}
+          key={name + "_" + url}
         >
           <div className="w-14 h-14 relative">
             <Image
-              src={each.url}
-              alt={each.name}
+              src={url}
+              alt={name}
               fill
               className="rounded-md object-cover object-center"
             />
           </div>
           <div className="flex flex-col justify-evenly h-full">
-            <span className="font-semibold text-white/80 text-sm">
-              {each.name}
+            <span className="font-semibold text-white/80 text-sm">{name}</span>
+            <span className="text-xs text-white/60">
+              {size > 1000 ? `${size / 1000}MB` : `${size}KB`}
             </span>
-            <span className="text-xs text-white/60">{each.size}MB</span>
           </div>
           <div className="ml-auto">
             <Button size="sm">View File</Button>
