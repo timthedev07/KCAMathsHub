b244e13d0aafb19b169da5f0c5d5cad126485ea2
timthedev07 <timpersonal07@gmail.com>
2023-12-09 15:08:18 +0100

    update domaint to use cloudfront

--- app/next.config.js
+++ app/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      `${process.env.S3_BUCKET_NAME}.s3-${process.env.AWS_REGION}.amazonaws.com/`,
+      `${process.env.S3_CLOUDFRONT_DOMAIN}`,
       "placehold.co",
       "lh3.googleusercontent.com",
     ].map((each) => {
