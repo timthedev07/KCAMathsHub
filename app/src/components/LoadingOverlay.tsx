ea4a71123a84891273707f0499bed16f5b834ea8
timthedev07 <timpersonal07@gmail.com>
2023-12-10 23:16:21 +0100

    fix overlay width

--- app/src/components/LoadingOverlay.tsx
+++ app/src/components/LoadingOverlay.tsx
@@ -9,11 +9,11 @@ interface LoadingOverlayProps {
 export const LoadingOverlay: FC<LoadingOverlayProps> = ({ isLoading }) => {
   return (
     <div
-      className={`transition justify-center items-center p-0 m-0 duration-300 w-min h-screen top-0 left-0 fixed bg-black/60 ${
+      className={`transition justify-center items-center p-0 m-0 duration-300 w-full h-screen top-0 left-0 fixed bg-black/80 ${
         isLoading ? "z-20 opacity-100 flex" : "-z-20 opacity-0 none"
       }`}
     >
-      <LoadingSpin size="lg" />
+      <LoadingSpin size="xl" />
     </div>
   );
 };
