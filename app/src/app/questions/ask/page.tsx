7bc78d2728faff0888a9f9f63ecab605132549ba
timthedev07 <timpersonal07@gmail.com>
2023-12-10 19:48:53 +0100

    use access guard on asking page

--- app/src/app/questions/ask/page.tsx
+++ app/src/app/questions/ask/page.tsx
@@ -1,25 +1,16 @@
 import { FC } from "react";
-import { getServerSession } from "../../../lib/authoptions";
 import { QuestionForm } from "../../../components/QuestionForm";
+import { WithSessionProps } from "../../../types/withSessionPage";
+import { withAccessGuard } from "../../../lib/accessGuard";
 
-const UnauthenticatedPage: FC = () => {
-  return <>HEY</>;
-};
-
-const AskPage: FC = async ({}) => {
-  const session = await getServerSession();
+const AskPage: FC<WithSessionProps> = async ({ session }) => {
   const u = session?.user;
-
-  if (!u) {
-    return <UnauthenticatedPage />;
-  }
 
   return (
     <div>
-      <h1 className="w-full text-4xl font-bold pl-10 pt-10">Ask a question today!</h1>
       <QuestionForm userId={u.id} />
     </div>
   );
 };
 
-export default AskPage;
+export default await withAccessGuard(AskPage, ["inquirer"]);
