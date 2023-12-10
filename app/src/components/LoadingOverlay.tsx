718ee77bcea54f0badeee20b85be37e38170df38
timthedev07 <timpersonal07@gmail.com>
2023-12-10 21:07:06 +0100

    add loading overlay

---
+++ app/src/components/LoadingOverlay.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { FC } from "react";
+import { LoadingSpin } from "./LoadingSpin";
+
+interface LoadingOverlayProps {
+  isLoading: boolean;
+}
+
+export const LoadingOverlay: FC<LoadingOverlayProps> = ({ isLoading }) => {
+  return (
+    <div
+      className={`transition justify-center items-center p-0 m-0 duration-300 w-min h-screen top-0 left-0 fixed bg-black/60 ${
+        isLoading ? "z-20 opacity-100 flex" : "-z-20 opacity-0 none"
+      }`}
+    >
+      <LoadingSpin size="lg" />
+    </div>
+  );
+};
