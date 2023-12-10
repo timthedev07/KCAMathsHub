91e8999f0c3cb4128259c5671ab674e4f673d189
timthedev07 <timpersonal07@gmail.com>
2023-12-10 23:16:29 +0100

    make deletable

--- app/src/components/QCategoryBadge.tsx
+++ app/src/components/QCategoryBadge.tsx
@@ -1,8 +1,10 @@
 import { FC } from "react";
+import { RxCross2 } from "react-icons/rx";
 
 interface QCategoryBadgeProps {
   name: string;
   ind: number;
+  onDelete?: Function;
 }
 
 const colors = [
@@ -13,14 +15,40 @@ const colors = [
   "bg-rose-600/80",
 ];
 
-export const QCategoryBadge: FC<QCategoryBadgeProps> = ({ name, ind }) => {
-  return (
+const hoverColors = [
+  "hover:bg-amber-600/70",
+  "hover:bg-lime-600/70",
+  "hover:bg-cyan-600/70",
+  "hover:bg-indigo-600/70",
+  "hover:bg-rose-600/70",
+];
+
+export const QCategoryBadge: FC<QCategoryBadgeProps> = ({
+  name,
+  ind,
+  onDelete,
+}) => {
+  return !onDelete ? (
     <span
       className={`rounded-lg text-xs text-white/80 px-3 font-semibold flex justify-center items-center h-6 ${
         colors[ind % colors.length]
       }`}
     >
       {name}
+    </span>
+  ) : (
+    <span
+      onClick={() => {
+        onDelete();
+      }}
+      className={`transition duration-200 cursor-pointer ${
+        hoverColors[ind & colors.length]
+      } rounded-lg text-xs text-white/80 px-3 gap-2 font-semibold flex justify-center items-center h-7 ${
+        colors[ind % colors.length]
+      }`}
+    >
+      {name}
+      <RxCross2 className="w-4 h-4" />
     </span>
   );
 };
