c0d941a6995d1b11cc3d02f39eca1afb5a9f1871
timthedev07 <timpersonal07@gmail.com>
2023-12-14 23:48:19 +0100

    fix

--- app/src/components/basis/user-expandable/index.tsx
+++ app/src/components/basis/user-expandable/index.tsx
@@ -21,13 +21,13 @@ export const UserExpandable: FC<UserExpandableProps> = ({
         onClick={() => {
           setIsOpen((prev) => !prev);
         }}
-        className="absolute left-0 top-0 w-[2400px] h-[10000px]"
+        className="absolute left-0 top-0 w-full h-full"
       ></div>
-      <div className="fixed right-4 top-20 max-w-[368px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 pt-10 px-8">
-        <div className="flex flex-col gap-2 items-center pb-10">
+      <div className="fixed right-4 top-20 max-w-[368px] border rounded-lg shadow bg-gray-900/40 border-gray-700/30 pt-10 px-8">
+        <div className="flex flex-col gap-3 items-center pb-10">
           {/* <Avatar img={user.image || undefined} size="30px" rounded /> */}
           <ProfileImgDisplay src={user.image} className="w-20 h-20" />
-          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white font-mono">
+          <h5 className="mb-1 text-lg font-medium text-gray-900 dark:text-white font-mono">
             {user.username}
           </h5>
           <BadgesDisplay center roles={user.roles} />
