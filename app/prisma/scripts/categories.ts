61cdf9f25fbbd64f1623c34176f62b73ab87f2ed
timthedev07 <timpersonal07@gmail.com>
2023-12-14 22:51:06 +0100

    add script for populating db

---
+++ app/prisma/scripts/categories.ts
@@ -0,0 +1,12 @@
+import { categories } from "../../src/categories.json";
+import prisma from "../../src/db";
+
+(async () => {
+  const c = categories;
+  await prisma.questionCategory.deleteMany();
+  await prisma.questionCategory.createMany({
+    data: c.map((name) => ({ name })),
+  });
+  await prisma.$disconnect();
+  console.log("\x1b[32m%s\x1b[0m", "Categories successfully populated.");
+})();
