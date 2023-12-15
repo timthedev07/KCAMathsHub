23bd7dd37cba04532aa3075c7162b1df3ef0daae
timthedev07 <timpersonal07@gmail.com>
2023-12-15 15:54:18 +0100

    fix

--- app/src/app/questions/ask/page.tsx
+++ app/src/app/questions/ask/page.tsx
@@ -8,7 +8,7 @@ const AskPage: FC<WithSessionProps> = async ({ session }) => {
 
   return (
     <div className="flex flex-col">
-      <h1 className="px-2 md:px-8 lg:px-16 mt-16 w-full text-start text-4xl font-semibold">
+      <h1 className="px-2 md:px-8 lg:px-16 mt-12 w-full text-start text-4xl font-semibold">
         Ask a Question
       </h1>
       <QuestionForm userId={u.id} />
