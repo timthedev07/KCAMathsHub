c5de443174cc739417f2772c122b026a8944ed2f
timthedev07 <timpersonal07@gmail.com>
2023-12-15 15:53:52 +0100

    add title

--- app/src/app/questions/ask/page.tsx
+++ app/src/app/questions/ask/page.tsx
@@ -7,7 +7,10 @@ const AskPage: FC<WithSessionProps> = async ({ session }) => {
   const u = session?.user;
 
   return (
-    <div>
+    <div className="flex flex-col">
+      <h1 className="px-2 md:px-8 lg:px-16 mt-16 w-full text-start text-4xl font-semibold">
+        Ask a Question
+      </h1>
       <QuestionForm userId={u.id} />
     </div>
   );
