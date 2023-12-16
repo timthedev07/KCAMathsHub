e111ac2f97e280fff54d21b7ef3404edf7fdc86f
timthedev07 <timpersonal07@gmail.com>
2023-12-17 00:09:56 +0100

    update response creating helpers

--- app/src/trpc/createError.ts
+++ app/src/trpc/createError.ts
@@ -1,7 +1,11 @@
 export const createError = (message: string, code: number = 400) => {
-  return { message, code, success: false };
+  return { message, code, data: null, success: false };
 };
 
-export const createSuccessResponse = (message: string, code: number = 200) => {
-  return { message, code, success: true };
+export const createSuccessResponse = <T>(
+  message: string,
+  data: T,
+  code: number = 200
+) => {
+  return { message, code, data, success: true };
 };
