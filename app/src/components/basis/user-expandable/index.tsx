d9992db73c23bc196c1864c11ab7374ae5cdd55d
timthedev07 <timpersonal07@gmail.com>
2023-12-17 00:27:32 +0100

    fix color

--- app/src/components/basis/user-expandable/index.tsx
+++ app/src/components/basis/user-expandable/index.tsx
@@ -21,17 +21,17 @@ export const UserExpandable: FC<UserExpandableProps> = ({
         onClick={() => {
           setIsOpen((prev) => !prev);
         }}
-        className="absolute left-0 top-0 w-full h-full"
+        className="absolute left-0 top-0 w-full h-screen z-20"
       ></div>
-      <div className="fixed right-4 top-20 max-w-[368px] border rounded-lg shadow bg-gray-900 border-gray-700/30 pt-10 px-8">
+      <div className="fixed right-4 top-20 shadow-2xl w-72 border rounded-lg shadow bg-[#12141a] border-gray-700/30 pt-10 px-8">
         <div className="flex flex-col gap-3 items-center pb-10">
           {/* <Avatar img={user.image || undefined} size="30px" rounded /> */}
           <ProfileImgDisplay src={user.image} className="w-20 h-20" />
-          <h5 className="mb-1 text-lg font-medium text-gray-900 dark:text-white font-mono">
+          <h5 className="mb-1 text-lg  text-white/80 font-mono">
             {user.username}
           </h5>
           <BadgesDisplay center roles={user.roles} />
-          <div className="flex mt-4 md:mt-6 gap-4">
+          <div className="flex mt-6 gap-4">
             <Button>
               <a href="/user/profile">Profile</a>
             </Button>
