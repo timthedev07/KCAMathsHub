be9a85b34aad5903e6d5a8a8e9d06dcd68d04312
timthedev07 <timpersonal07@gmail.com>
2023-12-17 22:23:31 +0100

    new helper

--- app/src/lib/isTeacher.ts
+++ app/src/lib/isTeacher.ts
@@ -1,1 +1,20 @@
 export const isTeacher = (email: string) => email.includes("@kings.education");
+
+const mathDepartment = [
+  "julia.galiana",
+  "sergio.balaguer",
+  "daria.malpart",
+  "stephen.lowe",
+  "doris.nistorica",
+  "sharon.doyle",
+  "ana.perez",
+];
+
+export const isMathTeacher = (email: string) => {
+  for (const t of mathDepartment) {
+    if (email.includes(t)) {
+      return true;
+    }
+  }
+  return false;
+};
