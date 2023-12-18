b30aca73da106f1f358a77a9eba08369dd7d8eb9
timthedev07 <timpersonal07@gmail.com>
2023-12-19 00:15:56 +0100

    update quid page display

--- app/src/app/questions/[quid]/page.tsx
+++ app/src/app/questions/[quid]/page.tsx
@@ -144,15 +144,14 @@ const Question: NextPage<Props> = async ({ params: { quid } }) => {
             />
           </div>
 
-          <h2 className="font-semibold text-3xl">Attachments</h2>
+          {attachments.length ? (
+            <h2 className="font-semibold text-3xl">Attachments</h2>
+          ) : null}
 
           <AttachmentList attachments={attachments} />
         </div>
-        <div className="min-w-[300px] max-w-[700px] w-full py-24 md:px-0 px-12">
-          <h2 className="font-semibold text-3xl">Answers</h2>
-          <AnswersDisplay quid={quid} />
-        </div>
-        <hr className="h-[1px] border-0 bg-slate-400/10 mx-auto w-11/12 my-20" />
+        <AnswersDisplay quid={quid} />
+        <hr className="h-[1px] border-0 bg-slate-400/10 mx-auto w-11/12" />
         <div className="min-w-[300px] max-w-[700px] w-full py-24 md:px-0 px-12">
           {u && (isAnswerer || isOwner) ? (
             <AnswerForm operationType="answer" quid={id} uid={u.id} />
