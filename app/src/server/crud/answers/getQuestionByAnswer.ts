2e858b0fcfd4fb8302c9999697ef72c1635802ea
timthedev07 <timpersonal07@gmail.com>
2023-12-18 19:15:14 +0100

    remove unnecessary query

--- app/src/server/crud/answers/getQuestionByAnswer.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { getQByAnswer } from "../../shared/getQ";
-import { publicProcedure } from "../../trpc";
-
-export const getQuestionByAnswer = publicProcedure
-  .input(z.object({ aid: z.string() }))
-  .query(({ input: { aid } }) => {
-    return getQByAnswer(aid);
-  });
