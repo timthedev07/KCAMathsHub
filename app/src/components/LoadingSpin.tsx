3871fa1c6eee31fabaf1f583f5cfa551dfe9b4e5
timthedev07 <timpersonal07@gmail.com>
2023-12-19 21:25:18 +0100

    add prop

--- app/src/components/LoadingSpin.tsx
+++ app/src/components/LoadingSpin.tsx
@@ -3,11 +3,17 @@ import { FC } from "react";
 
 interface LoadingSpinProps {
   size?: "xs" | "sm" | "md" | "lg" | "xl";
+  className?: string;
 }
 
-export const LoadingSpin: FC<LoadingSpinProps> = ({ size = "md" }) => {
+export const LoadingSpin: FC<LoadingSpinProps> = ({
+  size = "md",
+  className = "",
+}) => {
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div
+      className={`w-full h-full flex justify-center items-center ${className}`}
+    >
       <Spinner size={size} />
     </div>
   );
