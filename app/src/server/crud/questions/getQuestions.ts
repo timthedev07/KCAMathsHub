b6b99c3d9dc24ea75ddb98149df0a326de5ad917
timthedev07 <timpersonal07@gmail.com>
2023-12-20 21:29:18 +0100

    set limit

--- app/src/server/crud/questions/getQuestions.ts
+++ app/src/server/crud/questions/getQuestions.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { HOMEPAGE_QUESTION_PAGE_SIZE } from "../../../constants/pagination";
 import prisma from "../../../db";
 import { StudentStages } from "../../../types/StudentStage.d";
 import { publicProcedure } from "../../trpc";
@@ -7,13 +8,14 @@ export const getQuestions = publicProcedure
   .input(
     z.object({
       category: z.enum(StudentStages).optional(), // undefined => all
-      limit: z.number().max(50).min(10).optional().default(20),
       sortBy: z.enum(["timestamp"]).optional().default("timestamp"),
       order: z.enum(["asc", "desc"]).optional().default("desc"),
       cursor: z.string().nullish(),
     })
   )
-  .query(async ({ input: { category, limit, sortBy, order, cursor } }) => {
+  .query(async ({ input: { category, sortBy, order, cursor } }) => {
+    const limit = HOMEPAGE_QUESTION_PAGE_SIZE;
+
     try {
       const questions = await prisma.question.findMany({
         take: limit + 1,
