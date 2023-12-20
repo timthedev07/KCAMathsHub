dcb6044bdbb33edbda227a99cf9342c602f97b0f
timthedev07 <timpersonal07@gmail.com>
2023-12-20 22:18:45 +0100

    add helper

---
+++ app/src/lib/splitAtWord.ts
@@ -0,0 +1,21 @@
+export const splitAtWord = (str: string, maxCharacters: number) => {
+  const words = str.split(" ");
+
+  let i = 0;
+  const acceptedWords = [];
+
+  for (const word of words) {
+    let wordShouldAccepted = true;
+    for (const _ of word) {
+      i++;
+      if (i > maxCharacters) {
+        wordShouldAccepted = false;
+        break;
+      }
+    }
+    if (!wordShouldAccepted) break;
+    acceptedWords.push(word);
+  }
+
+  return acceptedWords.join(" ");
+};
