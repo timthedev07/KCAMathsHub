a74bb8bce25e7600e09f5cf4fb7875e8e41b850f
timthedev07 <timpersonal07@gmail.com>
2023-12-22 01:50:45 +0100

    complete useautosave

---
+++ app/src/hooks/useAutoSave.ts
@@ -0,0 +1,58 @@
+"use client";
+
+import equals from "fast-deep-equal";
+import { useEffect, useRef, useState } from "react";
+
+interface AutoSaveOptions<T> {
+  key: string;
+  data: T;
+  delay?: number;
+}
+
+export const initFromAutoSaveStorage = <T>(key: string) => {
+  if (typeof window === "undefined") return;
+  const stored = localStorage.getItem(key);
+  if (!stored) return null;
+  return JSON.parse(stored) as T;
+};
+
+type SaveStateType = "saving" | "saved" | "waiting";
+
+export const useAutoSave = <T extends string | Object>({
+  data,
+  key,
+  delay = 500,
+}: AutoSaveOptions<T>) => {
+  const prevData = useRef(data);
+  const [saveState, setSaveState] = useState<SaveStateType>("saved");
+
+  useEffect(() => {
+    if (saveState === "saved" && !equals(prevData.current, data)) {
+      setSaveState("waiting");
+    }
+    prevData.current = data;
+  }, [data, saveState]);
+
+  const dataHasChanged = saveState === "waiting";
+
+  useEffect(() => {
+    if (dataHasChanged) {
+      const t = setTimeout(() => {
+        if (typeof window === "undefined") return;
+        localStorage.setItem(key, JSON.stringify(data));
+        setSaveState("saved");
+      }, delay);
+
+      return () => {
+        clearTimeout(t);
+      };
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [delay, dataHasChanged, key]);
+
+  const clearStorage = () => {
+    localStorage.removeItem(key);
+  };
+
+  return { saveState, clearStorage };
+};
