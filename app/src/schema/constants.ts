ee60be246ff1a131eb672c91ce338932d12160b3
timthedev07 <timpersonal07@gmail.com>
2023-12-22 12:02:11 +0100

    rename file

---
+++ app/src/schema/constants.ts
@@ -0,0 +1,5 @@
+export type LimitType = [number, number];
+export const TITLE_LIMIT: LimitType = [8, 128];
+export const CONTENT_LIMIT: LimitType = [50, 5000];
+export const ANSWER_CONTENT_LIMIT: LimitType = [50, 5000];
+export const MODERATION_LIMIT: LimitType = [0, 1000];
--- app/src/schema/vars.ts
+++
@@ -1,3 +0,0 @@
-export const TITLE_LIMIT = [8, 128];
-export const CONTENT_LIMIT = [50, 5000];
-export const ANSWER_CONTENT_LIMIT = [50, 5000];
