6da0f65965181eb9e58aa310a1726417e49066bc
timthedev07 <timpersonal07@gmail.com>
2023-12-23 00:03:15 +0100

    fix helper

--- app/src/lib/handlePrismaError.ts
+++ app/src/lib/handlePrismaError.ts
@@ -1,9 +1,14 @@
 import { Prisma } from "@prisma/client";
 import { createError } from "../trpc/createError";
 
-export const handlePrismaError = (e: unknown, message?: string, log = true) => {
+export const handlePrismaError = (
+  e: unknown,
+  message?: string,
+  unknownErrMessage: string = "Unknown error",
+  log = true
+) => {
   if (e instanceof Prisma.PrismaClientKnownRequestError)
     return createError(message ? message : e.message);
   if (log) console.log(e);
-  return createError("Unknown error", 500);
+  return createError(unknownErrMessage, 500);
 };
