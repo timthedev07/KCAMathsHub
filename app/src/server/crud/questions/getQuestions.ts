3436dbf7dc9e03f535c81a46a5edc479be6dfdd0
timthedev07 <timpersonal07@gmail.com>
2023-12-23 23:43:58 +0100

    i feel fucked

--- app/src/server/crud/questions/getQuestions.ts
+++ app/src/server/crud/questions/getQuestions.ts
@@ -2,62 +2,76 @@ import { z } from "zod";
 import { HOMEPAGE_QUESTION_PAGE_SIZE } from "../../../constants/pagination";
 import prisma from "../../../db";
 import { truncateAtWord } from "../../../lib/truncateAtWord";
-import { StudentStages } from "../../../types/StudentStage";
+import { StudentStageType, StudentStages } from "../../../types/StudentStage";
 import { publicProcedure } from "../../trpc";
 
-export const getQuestions = publicProcedure
-  .input(
-    z.object({
-      category: z.enum(StudentStages).optional(), // undefined => all
-      sortBy: z.enum(["timestamp"]).optional().default("timestamp"),
-      order: z.enum(["asc", "desc"]).optional().default("desc"),
-      cursor: z.string().nullish(),
-    })
-  )
-  .query(async ({ input: { category, sortBy, order, cursor } }) => {
-    const limit = HOMEPAGE_QUESTION_PAGE_SIZE;
+export const GetQSSchema = z.object({
+  category: z.enum(StudentStages).optional(), // undefined => all
+  sortBy: z.enum(["timestamp"]).optional().default("timestamp"),
+  order: z.enum(["asc", "desc"]).optional().default("desc"),
+  cursor: z.string().nullish(),
+});
 
-    try {
-      const questions = (
-        await prisma.question.findMany({
-          take: limit + 1,
-          where: {
-            category,
-          },
-          orderBy: {
-            [sortBy]: order,
-          },
-          include: {
-            questioner: { select: { username: true, image: true } },
-            categories: { select: { name: true } },
-          },
-          cursor: cursor
-            ? {
-                id: cursor,
-              }
-            : undefined,
-        })
-      ).map((each) => ({
-        ...each,
-        title: truncateAtWord(each.title, 60),
-        content: truncateAtWord(each.content.replaceAll(/[`*_]/gi, ""), 200),
-      }));
+export const getQS = async ({
+  category,
+  sortBy = "timestamp",
+  order = "desc",
+  cursor,
+}: {
+  category?: StudentStageType;
+  sortBy?: "timestamp";
+  order?: "asc" | "desc";
+  cursor?: string | null;
+}) => {
+  const limit = HOMEPAGE_QUESTION_PAGE_SIZE;
 
-      let nextCursor: typeof cursor = undefined;
-      if (questions.length > limit) {
-        const nextItem = questions.pop();
-        nextCursor = nextItem!.id;
-      }
+  try {
+    const questions = (
+      await prisma.question.findMany({
+        take: limit + 1,
+        where: {
+          category,
+        },
+        orderBy: {
+          [sortBy]: order,
+        },
+        include: {
+          questioner: { select: { username: true, image: true } },
+          categories: { select: { name: true } },
+        },
+        cursor: cursor
+          ? {
+              id: cursor,
+            }
+          : undefined,
+      })
+    ).map((each) => ({
+      ...each,
+      title: truncateAtWord(each.title, 60),
+      content: truncateAtWord(each.content.replaceAll(/[`*_]/gi, ""), 200),
+    }));
 
-      return {
-        questions,
-        nextCursor,
-      };
-    } catch (e) {
-      console.log(e);
-      return {
-        questions: [],
-        nextCursor: undefined,
-      };
+    let nextCursor: typeof cursor = undefined;
+    if (questions.length > limit) {
+      const nextItem = questions.pop();
+      nextCursor = nextItem!.id;
     }
+
+    return {
+      questions,
+      nextCursor,
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      questions: [],
+      nextCursor: undefined,
+    };
+  }
+};
+
+export const getQuestions = publicProcedure
+  .input(GetQSSchema)
+  .query(async ({ input }) => {
+    return getQS(input);
   });
