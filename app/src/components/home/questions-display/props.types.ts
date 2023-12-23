a03db1063a1d4ef4b95b19cef339ac67cbe36dd2
timthedev07 <timpersonal07@gmail.com>
2023-12-23 23:44:13 +0100

    use type in prop

--- app/src/components/home/questions-display/props.types.ts
+++ app/src/components/home/questions-display/props.types.ts
@@ -1,8 +1,10 @@
 import { inferProcedureOutput } from "@trpc/server";
 import { getQuestions } from "../../../server/crud/questions/getQuestions";
+import { Query } from "../types";
 
 export type QueryOutput = inferProcedureOutput<typeof getQuestions>;
 
 export interface InfiniteScrollingDisplayProps {
   initialData: QueryOutput["questions"];
+  query: Partial<Query>;
 }
