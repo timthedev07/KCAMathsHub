2a7137cba9d26de71a831fb9bc2c53f2cedd90cc
timthedev07 <timpersonal07@gmail.com>
2023-12-23 22:19:30 +0100

    fix ugly page

--- app/src/app/not-found.tsx
+++ app/src/app/not-found.tsx
@@ -8,7 +8,7 @@ import { NextPage } from "../types/nextpage";
 const NotFound: NextPage = () => {
   const router = useRouter();
   return (
-    <div className="bg-gradient-to-br from-black to-[#130F40] to-50% w-full h-[90vh] flex flex-col gap-10 justify-center items-center pt-24 text-center">
+    <div className="w-full h-[90vh] flex flex-col gap-10 justify-center items-center text-center">
       <h1 className="text-red-400 font-bold text-8xl font-mono">404</h1>
       <h4 className="text-red-200/80 text-lg font-semibold">Page not Found</h4>
       <div className="w-full flex justify-center items-center gap-10">
@@ -16,12 +16,12 @@ const NotFound: NextPage = () => {
           onClick={() => {
             router.back();
           }}
-          gradientDuoTone="purpleToBlue"
+          color="info"
         >
           Previous
         </Button>
         <Link href="/" prefetch={false} passHref>
-          <Button gradientDuoTone="greenToBlue">Home</Button>
+          <Button color="success">Home</Button>
         </Link>
       </div>
     </div>
