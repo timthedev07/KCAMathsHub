721f0ef577528eea98d979ef0985a0acaf502fd5
timthedev07 <timpersonal07@gmail.com>
2023-12-23 01:15:22 +0100

    safety

--- app/src/lib/getCurrYear.ts
+++ app/src/lib/getCurrYear.ts
@@ -1,8 +1,10 @@
 export const getCurrYear = (joinedDate: Date, joinedYear: number) => {
+  const j = new Date(joinedDate);
+
   const nextAug1st =
-    joinedDate.getMonth() + 1 >= 8
-      ? new Date(joinedDate.getFullYear() + 1, 9, 1)
-      : new Date(joinedDate.getFullYear(), 9, 1);
+    j.getMonth() + 1 >= 8
+      ? new Date(j.getFullYear() + 1, 9, 1)
+      : new Date(j.getFullYear(), 9, 1);
 
   if (Date.now() >= nextAug1st.valueOf()) {
     return joinedYear + 1;
