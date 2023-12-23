ba1669701d46e5f5b29e3c446d33f94b8c6f9c49
timthedev07 <timpersonal07@gmail.com>
2023-12-23 20:55:04 +0100

    add limiting condition for deletion

--- app/src/server/crud/answers/deleteAnswer.ts
+++ app/src/server/crud/answers/deleteAnswer.ts
@@ -8,11 +8,17 @@ export const deleteAnswer = publicProcedure
   .input(z.object({ aid: z.string() }))
   .mutation(async ({ input: { aid } }) => {
     try {
-      await prisma.answer.delete({ where: { id: aid } });
+      await prisma.answer.delete({
+        where: {
+          id: aid,
+          accepted: false,
+          moderated: false,
+        },
+      });
       return createSuccessResponse("Answer deleted");
     } catch (e) {
       if (e instanceof Prisma.PrismaClientKnownRequestError) {
-        return createError(e.message);
+        return createError("Answer cannot be deleted");
       } else {
         return createError("Unknown error", 500);
       }
