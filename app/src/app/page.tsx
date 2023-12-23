e0221cbac7a13d81e5a266a9b2ae7f589edb5a86
timthedev07 <timpersonal07@gmail.com>
2023-12-23 21:56:41 +0100

    responsive homepage layout

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,16 +1,32 @@
+import { KSList } from "../components/home/ks-list";
 import { HPQuestionsDisplay } from "../components/home/questions-display";
 import { SSRCaller } from "../server";
+import { StudentStageType, StudentStages } from "../types/StudentStage";
 
-const getProps = async () => {
-  return await SSRCaller.getQuestions({});
+const getProps = async (k?: string | null) => {
+  let _k;
+  if (k && StudentStages.includes(k as any)) {
+    _k = k as StudentStageType;
+  }
+
+  const res = await SSRCaller.getQuestions({ category: _k });
+  console.log(res);
+  return res;
 };
 
-const Home = async () => {
-  const { questions } = await getProps();
+const Home = async ({
+  searchParams: { k },
+}: {
+  searchParams: { k?: string | null };
+}) => {
+  const { questions } = await getProps(k);
+
   return (
-    <div className="flex">
-      <aside className="w-4/12 lg:w-3/12 border-r border-slate-600/20 min-h-[90vh]"></aside>
-      <div className="w-8/12 lg:w-1/2 flex flex-col py-8 px-12 md:px-12 lg:px-16 gap-4 items-center">
+    <div className="flex lg:flex-row flex-col">
+      <aside className="lg:w-3/12 h-64 border-b lg:border-r border-slate-600/20 lg:min-h-[90vh]">
+        <KSList />
+      </aside>
+      <div className="w-full md:w-9/12 md:mx-auto lg:mx-[unset] lg:w-1/2 flex flex-col py-8 px-12 md:px-12 lg:px-16 gap-4 items-center">
         <HPQuestionsDisplay initialData={questions} />
       </div>
       <aside className="w-3/12 border-l border-slate-600/20 min-h-[90vh] hidden lg:block"></aside>
