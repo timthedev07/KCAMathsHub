279a29381fa02f08a82199dbc4c6d4b9bf2d5b4e
timthedev07 <timpersonal07@gmail.com>
2023-12-23 23:43:50 +0100

    update nav item component

--- app/src/components/basis/nav/NavItem.tsx
+++ app/src/components/basis/nav/NavItem.tsx
@@ -1,14 +1,15 @@
+import type { Url } from "next/dist/shared/lib/router/router";
 import Link from "next/link";
 import { FC } from "react";
 
 interface NavItemProps {
-  url: string;
+  url: Url;
   text: string;
 }
 
 export const NavItem: FC<NavItemProps> = ({ text, url }) => {
   return (
-    <li className="flex justify-center items-center text-center">
+    <li className="flex justify-center items-center text-center uppercase transition duration-200 text-white/60 hover:text-white/90">
       <Link href={url}>{text}</Link>
     </li>
   );
