d389bc4a0a2359c032695ecd9124966c5d932042
timthedev07 <timpersonal07@gmail.com>
2023-12-23 16:36:22 +0100

    update mutation

--- app/src/server/crud/questions/askQuestion.ts
+++ app/src/server/crud/questions/askQuestion.ts
@@ -1,8 +1,11 @@
 import { TRPCError } from "@trpc/server";
 import prisma from "../../../db";
+import { roleChecker } from "../../../lib/accessGuard";
 import { classifyKSCategory } from "../../../lib/classifyKSCategory";
 import { getCurrYear } from "../../../lib/getCurrYear";
+import { handlePrismaError } from "../../../lib/handlePrismaError";
 import { AskSubmissionSchema } from "../../../schema/ask";
+import { createError } from "../../../trpc/createError";
 import { publicProcedure } from "../../trpc";
 
 export const askQuestion = publicProcedure
@@ -11,17 +14,16 @@ export const askQuestion = publicProcedure
     async ({
       input: { attachmentIds, content, title, userId, anonymous, categories },
     }) => {
-      const u = await prisma.user.findFirst({ where: { id: userId } });
+      const u = await prisma.user.findFirst({
+        where: { id: userId },
+        include: { roles: { select: { name: true } } },
+      });
 
-      if (!u)
-        throw new TRPCError({ code: "UNAUTHORIZED", message: "Invalid user" });
-
-      if (!u.joinedYear) {
-        throw new TRPCError({
-          code: "FORBIDDEN",
-          message: "At this stage, only students can ask questions.",
-        });
-      }
+      if (
+        !u ||
+        !roleChecker(u.roles.map((each) => each.name) as any, ["inquirer"])
+      )
+        return createError("Unauthorized", 401);
 
       const attachments = await prisma.imageAttachment.findMany({
         where: { id: { in: attachmentIds } },
@@ -46,21 +48,21 @@ export const askQuestion = publicProcedure
               connect: attachments,
             },
             questioner: { connect: { id: userId } },
-            category: classifyKSCategory(u.joinedDate, u.joinedYear),
-            yearGroupAsked: getCurrYear(u.joinedDate, u.joinedYear),
+            category: u.joinedYear
+              ? classifyKSCategory(u.joinedDate, u.joinedYear)
+              : null,
+            yearGroupAsked: u.joinedYear
+              ? getCurrYear(u.joinedDate, u.joinedYear)
+              : null,
             anonymous,
             timestamp: new Date(),
             categories: { connect: categories.map((name) => ({ name })) },
           },
         });
         finalId = askedQuestion.id;
+        return finalId;
       } catch (err: any) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: err.message,
-        });
+        return handlePrismaError(err);
       }
-
-      return finalId;
     }
   );
