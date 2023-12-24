66e4d238539c348a4026268677737352c2b7405a
timthedev07 <timpersonal07@gmail.com>
2023-12-24 21:27:02 +0100

    preserve other params

--- app/src/components/basis/nav/NavItem.tsx
+++ app/src/components/basis/nav/NavItem.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import type { Url } from "next/dist/shared/lib/router/router";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 import { FC } from "react";
+import { searchParamsEntriesToObj } from "../../../lib/searchParamsEntriesToObj";
 
 interface NavItemProps {
   url: Url;
@@ -8,9 +12,25 @@ interface NavItemProps {
 }
 
 export const NavItem: FC<NavItemProps> = ({ text, url }) => {
+  const { entries } = useSearchParams();
+
   return (
     <li className="flex justify-center items-center text-center uppercase transition duration-200 text-white/60 hover:text-white/90">
-      <Link href={url}>{text}</Link>
+      <Link
+        href={
+          typeof url === "string"
+            ? url
+            : {
+                ...url,
+                query: {
+                  ...searchParamsEntriesToObj(entries()),
+                  ...(typeof url.query === "string" ? {} : url.query),
+                },
+              }
+        }
+      >
+        {text}
+      </Link>
     </li>
   );
 };
