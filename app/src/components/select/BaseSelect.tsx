3984a26c8387a7dbf6eb9fa92fd7dda09f73f2ea
timthedev07 <timpersonal07@gmail.com>
2023-12-25 17:52:42 +0100

    reset on route change

--- app/src/components/select/BaseSelect.tsx
+++ app/src/components/select/BaseSelect.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Listbox } from "@headlessui/react";
+import { useSearchParams } from "next/navigation";
 import type { FC } from "react";
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import {
   SharedTransition,
   getEntryClassName,
@@ -8,22 +10,35 @@ import {
   getOptionsUIClassName,
 } from "../shared-styling/inputMenu";
 
+type V = { value: string; displayName: string };
+
 export interface BaseSelectProps {
-  dataset: { value: string; displayName: string }[];
+  dataset: V[];
   onChange?: (_: string) => void;
-  defaultValue?: { value: string; displayName: string };
+  defaultValue?: V;
+  resetStateOnRouteChange?: () => V;
 }
 
 export const BaseSelect: FC<BaseSelectProps> = ({
   dataset,
   onChange,
   defaultValue,
+  resetStateOnRouteChange,
 }) => {
   const completeDataset: typeof dataset = [
     { value: "", displayName: "All" },
     ...dataset,
   ];
   const [selected, setSelected] = useState(defaultValue || completeDataset[0]);
+  const searchParams = useSearchParams();
+  const k = searchParams.get("k");
+
+  useEffect(() => {
+    if (resetStateOnRouteChange) {
+      setSelected(resetStateOnRouteChange());
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [k]);
 
   return (
     <Listbox
