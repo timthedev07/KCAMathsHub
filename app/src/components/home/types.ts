fd06226137bd75411f3352f701cec366df4550b7
timthedev07 <timpersonal07@gmail.com>
2023-12-25 17:16:28 +0100

    extract types

--- app/src/components/home/types.ts
+++ app/src/components/home/types.ts
@@ -1,4 +1,7 @@
 import { z } from "zod";
 import { GetQSSchema } from "../../server/crud/questions/getQuestions";
+import { NextPageParams } from "../../types/nextPageParam";
 
 export type Query = z.infer<typeof GetQSSchema>;
+
+export type HomePageParams = NextPageParams<{}, "q" | "k" | "c" | "u" | "y">;
