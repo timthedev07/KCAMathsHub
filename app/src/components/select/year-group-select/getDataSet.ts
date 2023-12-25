1ed6e71117dccf16cacd70d6bec2faa95e5bfc34
timthedev07 <timpersonal07@gmail.com>
2023-12-25 16:44:34 +0100

    fix

--- app/src/components/select/year-group-select/getDataSet.ts
+++ app/src/components/select/year-group-select/getDataSet.ts
@@ -3,7 +3,7 @@ const yearGroups = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 export const getYearGroupsByK = (k?: string) => {
   switch (k) {
     case "Primary":
-      return yearGroups.slice(1, 6);
+      return yearGroups.slice(0, 6);
     case "KS3":
       return yearGroups.slice(6, 9);
     case "KS4":
