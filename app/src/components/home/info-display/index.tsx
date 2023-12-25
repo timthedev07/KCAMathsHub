4a62d6423bc96ff5c93813200934966b6a028cb8
timthedev07 <timpersonal07@gmail.com>
2023-12-25 21:53:27 +0100

    shit complete home page

--- app/src/components/home/info-display/index.tsx
+++ app/src/components/home/info-display/index.tsx
@@ -1,3 +1,42 @@
+import Link from "next/link";
+
+import { FaInfoCircle } from "react-icons/fa";
+import { IoMdLink } from "react-icons/io";
+import { Section } from "./Section";
+
 export const HomePageInfoDisplay = () => {
-  return <></>;
+  return (
+    <div className="flex flex-col gap-10 p-8">
+      <Section colorScheme="blue" heading="Welcome" icon={FaInfoCircle}>
+        <div className="flex flex-col">
+          Welcome to KCAMathsHub, an internally maintained and used platform for
+          asking, answering, and moderating questions that students at any
+          stage, in any year group, encountered.
+        </div>
+      </Section>
+      <Section colorScheme="green" heading="Quick access" icon={IoMdLink}>
+        <ul className="list-disc list-inside pl-3">
+          <li>
+            <Link href="/docs">Docs</Link>
+          </li>
+          <li>
+            <Link href="/questions/ask">Ask question</Link>
+          </li>
+          <li>
+            <Link href="/user/profile">Profile</Link>
+          </li>
+        </ul>
+      </Section>
+      <Section colorScheme="orange" heading="Expectations" icon={FaInfoCircle}>
+        <p className="mb-3">
+          Students are expected to act sensibly and respectfully on the
+          platform.
+        </p>
+        <p>
+          Offensive/disruptive behavior will be recorded and reported. Read more
+          about legislations <Link href="/guide#legislation">here</Link>.
+        </p>
+      </Section>
+    </div>
+  );
 };
