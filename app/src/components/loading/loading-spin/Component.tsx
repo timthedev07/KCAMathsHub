8c91d4db0ab1edea2d8404840bad437e9e29ddc8
timthedev07 <timpersonal07@gmail.com>
2023-12-25 18:27:13 +0100

    get rid of fucking flowbite

--- app/src/components/loading/loading-spin/Component.tsx
+++ app/src/components/loading/loading-spin/Component.tsx
@@ -1,17 +1,26 @@
-import { Spinner } from "flowbite-react";
 import type { FC } from "react";
+import { AiOutlineLoading } from "react-icons/ai";
 
 interface LoadingSpinProps {
-  size?: "xs" | "sm" | "md" | "lg" | "xl";
+  size?: "sm" | "md" | "lg" | "xl";
   className?: string;
 }
+
+const sizeMap: Record<Required<LoadingSpinProps>["size"], string> = {
+  xl: "w-16 h-16",
+  lg: "w-12 h-12",
+  md: "w-8 h-8",
+  sm: "w-6 h-6",
+};
 
 const Component: FC<LoadingSpinProps> = ({ size = "md", className = "" }) => {
   return (
     <div
       className={`w-full h-full flex justify-center items-center ${className}`}
     >
-      <Spinner size={size} />
+      <AiOutlineLoading
+        className={`${sizeMap[size]} text-blue-600/80 animate-spin`}
+      />
     </div>
   );
 };
