c9a4bc3a2214b5ac6bec3a6299e543026d9a3aa7
timthedev07 <timpersonal07@gmail.com>
2023-12-25 14:00:37 +0100

    fix faking logic

--- app/prisma/scripts/dev/fakeq.ts
+++ app/prisma/scripts/dev/fakeq.ts
@@ -1,6 +1,19 @@
 import { categories } from "../../../src/categories";
 import prisma from "../../../src/db";
-import { StudentStages } from "../../../src/types/StudentStage";
+import { classifyKSCategoryByYear } from "../../../src/lib/classifyKSCategory";
+
+const paragraphs = [
+  `Differentiation is a fundamental concept in various fields, encompassing mathematics, biology, business, and education. In mathematics, it refers to the process of finding the rate at which a quantity changes concerning another, often expressed as the derivative. This tool is pivotal in understanding the behavior of functions and curves, enabling mathematicians and scientists to analyze patterns and make predictions. In biology, differentiation refers to the process by which cells become specialized and assume distinct functions in an organism, a crucial aspect of development and growth. In business, product differentiation is a strategy employed by companies to distinguish their offerings from competitors, fostering brand identity and customer loyalty. Moreover, differentiation in education involves tailoring instruction to meet the diverse needs of students, recognizing and accommodating various learning styles and abilities. In essence, differentiation underscores the dynamic nature of progress, adaptation, and individuality across disciplines, driving innovation and fostering excellence.`,
+  `Integration is a key concept that plays a central role in mathematics, science, and various other disciplines. In mathematics, integration is the inverse operation of differentiation, involving the accumulation of quantities or the determination of the area under a curve. It provides a powerful tool for calculating accumulated changes and analyzing the overall behavior of functions. In physics and engineering, integration is employed to solve problems related to motion, energy, and fluid dynamics, offering a means to comprehend complex systems and phenomena. Beyond mathematics and science, the term "integration" extends into social contexts, where it signifies the process of combining or coordinating different elements into a unified whole. This can be observed in societal integration, where diverse cultures, ideas, and individuals come together to form a cohesive and harmonious community. Overall, integration serves as a unifying force, whether in the realm of mathematics, physical sciences, or the broader tapestry of human interactions, contributing to a more comprehensive understanding of complex systems and fostering unity and synergy.`,
+  `De Moivre's Theorem simplifies complex number powers. For any complex number  z = r ( cos θ + i sin θ ) z=r(cosθ+isinθ) and positive integer  n n,  z n = r n ( cos ⁡ ( n θ ) + i sin ⁡ ( n θ ) ) z  n  =r  n  (cos(nθ)+isin(nθ)). This theorem is useful in math and physics for straightforward calculations with complex numbers.`,
+  `Composite functions involve combining two or more functions to create a new one. If you have functions  f ( x ) f(x) and  g ( x ) g(x), their composite function is  ( f ∘ g ) ( x ) (f∘g)(x) or  f ( g ( x ) ) f(g(x)). This concept is widely used in math to analyze relationships between variables and solve problems involving multiple processes or transformations.`,
+];
+
+function randInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function choose(choices: readonly string[]) {
   return choices[Math.floor(Math.random() * choices.length)];
@@ -22,25 +35,20 @@ function randomChoices(possibilities: string[]) {
   return final;
 }
 
-const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum risus justo, eu malesuada elit rhoncus sed. Nam sollicitudin ligula aliquam, pretium leo sit amet, vestibulum diam. Morbi ut orci volutpat, lacinia ipsum sagittis, consequat risus. Praesent a arcu magna. Integer in eros laoreet elit maximus tempor. Nam lobortis dui leo, sagittis malesuada ipsum vehicula sit amet. Aliquam aliquam maximus felis, et efficitur mi volutpat id. Curabitur eu elit purus. In faucibus condimentum aliquet. Praesent accumsan augue sit amet urna eleifend, ac vestibulum metus congue. Vestibulum tincidunt bibendum magna. Quisque luctus euismod dolor at convallis.
-
-Nulla cursus, mi sed rutrum condimentum, nulla ante commodo massa, ac egestas risus sapien non justo. Phasellus vitae ex nisi. Morbi id magna a diam cursus hendrerit non id orci. Nullam mollis sit amet quam a egestas. Aenean vehicula velit eget libero porttitor elementum imperdiet eget neque. Curabitur a erat cursus, placerat ligula eget, aliquet mi. Nulla dapibus tristique nisi sed venenatis. Pellentesque erat metus, finibus sit amet lobortis quis, semper placerat nibh. In commodo posuere libero, at varius velit luctus sed.
-
-Cras nec scelerisque sapien. Suspendisse ut risus eget tellus vehicula pretium id in leo. Quisque nec mollis mi, quis porta diam. Mauris pretium nulla tortor, eget mollis eros lobortis vel. Sed volutpat sed dolor ac rhoncus. Vestibulum id dolor a tellus eleifend varius. Cras eget neque sollicitudin, condimentum enim eu, cursus ex. Vestibulum eleifend, tortor ut consequat suscipit, tortor dolor lobortis libero, eu vestibulum magna libero ac sem. Sed tincidunt ipsum quis tortor varius, ut mattis orci sollicitudin.
-
-Maecenas quis sagittis enim. Vivamus nunc nunc, porta quis erat eget, mattis rutrum elit. Nulla in justo ac nisi faucibus commodo nec sit amet quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis vel lacinia urna. In pellentesque lacus feugiat arcu efficitur fermentum. Donec congue imperdiet lectus id tincidunt. Praesent ut aliquet quam. Fusce rutrum ipsum est, eu dignissim orci condimentum eget. Maecenas mattis consequat magna, id mattis ex tempor sed. Aenean elit nisl, semper rutrum augue a, luctus laoreet nulla. Pellentesque efficitur ullamcorper odio a egestas. Mauris luctus tristique arcu ut vehicula.
-
-Praesent porta, nibh a ullamcorper posuere, purus mauris pharetra massa, aliquam pretium nisi erat id massa. Duis sollicitudin ipsum in est molestie malesuada. Proin suscipit nisi a egestas porta. Donec dignissim nisi et erat lacinia, et condimentum libero sodales. Nam mauris urna, eleifend vel enim ut, blandit tempus enim. Fusce eu enim feugiat, ultrices enim vel, iaculis risus. Mauris eget fermentum ex, eget bibendum turpis. Curabitur et urna nunc. Maecenas convallis tincidunt luctus. Aenean eros nulla, imperdiet a accumsan in, finibus eu diam. Integer a lorem semper, efficitur nulla non, lacinia mauris. Sed finibus accumsan nibh, sed rhoncus felis commodo vel. Duis et vestibulum enim, ac egestas nibh. Vivamus sed quam a orci imperdiet ullamcorper posuere et nulla. Nam dictum viverra enim consectetur porttitor.`;
-
 (async () => {
+  await prisma.question.deleteMany();
+
   for (let i = 1; i <= 100; i++) {
+    const yeargroup = randInt(1, 13);
+
     await prisma.question.create({
       data: {
-        content: lorem,
+        content: choose(paragraphs),
         title: `Question ${i}`,
-        studentStage: choose(StudentStages),
-        yearGroupAsked: Math.floor(Math.random() * 13),
+        studentStage: classifyKSCategoryByYear(yeargroup),
+        yearGroupAsked: yeargroup,
         answered: Math.random() > 0.5 ? false : true,
+        anonymous: Math.random() > 0.5 ? false : true,
         categories: {
           connect: randomChoices(categories).map((c) => ({ name: c })),
         },
