6fa74cfb489acd2d714a036d13494c36db97bd7c
timthedev07 <timpersonal07@gmail.com>
2023-12-25 14:09:35 +0100

    split code

---
+++ app/src/components/home/validation.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const titleSearchValidation = z
+  .string()
+  .trim()
+  .min(5, "Type in at least 5 characters")
+  .max(48)
+  .transform((v) => {
+    return v
+      .toLowerCase()
+      .replaceAll(/[^-\w\d\s:]/gi, "")
+      .replaceAll(/(-)\1+/gi, "-")
+      .trim();
+  });
