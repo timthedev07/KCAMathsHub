03be4c5bb736252ceccd182acff2ab68d26af31a
timthedev07 <timpersonal07@gmail.com>
2023-12-25 17:23:27 +0100

    add helper

---
+++ app/src/lib/queryParamCompare.ts
@@ -0,0 +1,12 @@
+export const queryParamCompare = <T extends Object, P extends T>(
+  a: T,
+  b: P,
+  keys: (keyof T)[]
+) => {
+  for (const key of keys) {
+    if (a[key] !== b[key]) {
+      return false;
+    }
+  }
+  return true;
+};
