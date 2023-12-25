d1d78bbe884df6d8fedb1df8b8cef3c12aff3481
timthedev07 <timpersonal07@gmail.com>
2023-12-25 14:26:05 +0100

    add u search option

--- app/src/components/home/PageDisplay.tsx
+++ app/src/components/home/PageDisplay.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { inferProcedureOutput } from "@trpc/server";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { ChangeEventHandler, FC, useCallback, useRef, useState } from "react";
+import { ChangeEvent, FC, useCallback, useRef, useState } from "react";
 import { GrLinkTop } from "react-icons/gr";
 import { useDebouncedCallback } from "use-debounce";
-import { z } from "zod";
+import type { z } from "zod";
 import { getQuestions } from "../../server/crud/questions/getQuestions";
 import { CategoryAutoComplete } from "../categories/CategoryAutoComplete";
 import { Button } from "../reusable/Button";
@@ -12,23 +12,14 @@ import { Input } from "../reusable/Input";
 import { LabelErrorWrapper } from "../reusable/WithLabelWrapper";
 import { InputWait } from "./InputWait";
 import { InfiniteScrollingDisplay } from "./questions-display/InfiniteScrollingDisplay";
+import {
+  titleSearchValidation,
+  userSearchValidation,
+} from "./searchInputValidation";
 
 type Props = {
   questions: inferProcedureOutput<typeof getQuestions>["questions"];
 };
-
-const searchValidation = z
-  .string()
-  .trim()
-  .min(5, "Type in at least 5 characters")
-  .max(48)
-  .transform((v) => {
-    return v
-      .toLowerCase()
-      .replaceAll(/[^-\w\d\s:]/gi, "")
-      .replaceAll(/(-)\1+/gi, "-")
-      .trim();
-  });
 
 export const PageDisplay: FC<Props> = ({ questions }) => {
   const searchParams = useSearchParams();
@@ -36,10 +27,11 @@ export const PageDisplay: FC<Props> = ({ questions }) => {
   const pathname = usePathname();
   const ref = useRef<HTMLDivElement>(null);
   const [category, setCategory] = useState<string | null>(null);
-  const [errors, setErrors] = useState<{ search?: string }>({});
+  const [errors, setErrors] = useState<{ q?: string; u?: string }>({});
   const [inputLoading, setInputLoading] = useState<{
-    title?: boolean;
-    category?: string;
+    q?: boolean;
+    category?: boolean;
+    u?: boolean;
   }>({});
 
   const setParam = useCallback(
@@ -52,32 +44,38 @@ export const PageDisplay: FC<Props> = ({ questions }) => {
     [searchParams, pathname, replace]
   );
 
-  const handleSearchChange = useDebouncedCallback<
-    ChangeEventHandler<HTMLInputElement>
-  >((e) => {
-    if (e.target.value.length > 48) {
-      e.preventDefault();
-    }
-    const v = e.target.value;
-    if (v.length < 1) {
-      setParam("q");
-    }
+  const handleSearchChange = useDebouncedCallback(
+    (
+      e: ChangeEvent<HTMLInputElement>,
+      maxLen: number,
+      validationSchema: z.ZodEffects<z.ZodString, string, string>
+    ) => {
+      if (e.target.value.length > maxLen) {
+        e.preventDefault();
+      }
+      const key = e.target.name;
+      const v = e.target.value;
+      if (v.length < 1) {
+        setParam(key);
+      }
 
-    const parsed = searchValidation.safeParse(v);
-    if (parsed.success) {
-      setParam("q", parsed.data);
-      setErrors((prev) => ({
-        ...prev,
-        search: undefined,
-      }));
-    } else {
-      setErrors((prev) => ({
-        ...prev,
-        search: parsed.error.issues[0].message,
-      }));
-    }
-    setInputLoading((prev) => ({ ...prev, title: false }));
-  }, 800);
+      const parsed = validationSchema.safeParse(v);
+      if (parsed.success) {
+        setParam(key, parsed.data);
+        setErrors((prev) => ({
+          ...prev,
+          [key]: undefined,
+        }));
+      } else {
+        setErrors((prev) => ({
+          ...prev,
+          [key]: parsed.error.issues[0].message,
+        }));
+      }
+      setInputLoading((prev) => ({ ...prev, [key]: false }));
+    },
+    800
+  );
 
   return (
     <>
@@ -92,25 +90,24 @@ export const PageDisplay: FC<Props> = ({ questions }) => {
             label={
               <>
                 Title search{" "}
-                {inputLoading.title ? (
-                  <InputWait className="w-4 h-4 ml-3" />
-                ) : null}
+                {inputLoading.q ? <InputWait className="w-4 h-4 ml-3" /> : null}
               </>
             }
             className="w-9/12 mx-auto min-w-[180px] lg:mx-[unset] lg:w-full"
-            error={errors.search}
+            error={errors.q}
           >
             <Input
               maxLength={48}
               defaultValue={searchParams.get("q")?.toString()}
+              name="q"
               className="text-sm"
               widthClassName="w-full"
               onChange={(e) => {
                 if (e.target.value.length < 1) {
                   setParam("q");
                 }
-                setInputLoading((prev) => ({ ...prev, title: true }));
-                handleSearchChange(e);
+                setInputLoading((prev) => ({ ...prev, q: true }));
+                handleSearchChange(e, 48, titleSearchValidation);
               }}
             />
           </LabelErrorWrapper>
@@ -131,6 +128,37 @@ export const PageDisplay: FC<Props> = ({ questions }) => {
               selectedCategories={category ? [category] : []}
             />
           </LabelErrorWrapper>
+          <LabelErrorWrapper
+            labelFontSize="text-base"
+            label={
+              <>
+                By user{" "}
+                {inputLoading.u ? <InputWait className="w-4 h-4 ml-3" /> : null}
+              </>
+            }
+            className="w-9/12 mx-auto min-w-[180px] lg:mx-[unset] lg:w-full"
+            error={errors.u}
+          >
+            <Input
+              maxLength={32}
+              defaultValue={searchParams.get("u")?.toString()}
+              name="u"
+              className="text-sm"
+              widthClassName="w-full"
+              onChange={(e) => {
+                if (e.target.value.length < 1) {
+                  setParam("u");
+                }
+                setInputLoading((prev) => ({ ...prev, u: true }));
+                handleSearchChange(e, 32, userSearchValidation);
+              }}
+            />
+          </LabelErrorWrapper>
+          <LabelErrorWrapper
+            labelFontSize="text-base"
+            label="Sort by"
+            className="w-9/12 min-w-[180px] mx-auto lg:mx-[unset] lg:w-full"
+          ></LabelErrorWrapper>
         </aside>
         <div className=" w-full md:w-9/12 md:mx-auto lg:mx-[unset] lg:w-1/2 flex flex-col py-8 px-12 md:px-12 lg:px-16 gap-4 items-center">
           {questions.length > 0 ? (
