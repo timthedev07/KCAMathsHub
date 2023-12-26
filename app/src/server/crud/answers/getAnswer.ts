ee79ad71428fd6cdee44cfd894736b0b9d23b604
timthedev07 <timpersonal07@gmail.com>
2023-12-26 22:24:09 +0100

    preprocessing and expose more data

--- app/src/server/crud/answers/getAnswer.ts
+++ app/src/server/crud/answers/getAnswer.ts
@@ -1,22 +1,32 @@
 import { z } from "zod";
 import prisma from "../../../db";
+import { preprocessAttachmentsForEdit } from "../../helpers/preprocessAttachmentForEdit";
 import { publicProcedure } from "../../trpc";
 
 export const getAnswer = publicProcedure
   .input(z.object({ aid: z.string() }))
   .query(async ({ input: { aid } }) => {
-    return await prisma.answer.findFirst({
+    const a = await prisma.answer.findFirst({
       where: { id: aid },
       select: {
         accepted: true,
         anonymous: true,
-        answerer: { select: { username: true } },
+        answerer: { select: { username: true, image: true } },
         approved: true,
         moderated: true,
         timestamp: true,
         attachments: { select: { name: true, size: true, objKey: true } },
         content: true,
-        question: { select: { id: true } },
+        question: {
+          select: { id: true, categories: { select: { name: true } } },
+        },
       },
     });
+
+    return a
+      ? {
+          ...a,
+          attachments: preprocessAttachmentsForEdit(a?.attachments || []),
+        }
+      : null;
   });
