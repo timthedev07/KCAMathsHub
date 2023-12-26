4ff8243e2a48294d0000e34832d41590cafc64ee
timthedev07 <timpersonal07@gmail.com>
2023-12-26 14:03:32 +0100

    fix

--- app/src/svgs/sidebar/Hamburger.tsx
+++ app/src/svgs/sidebar/Hamburger.tsx
@@ -3,7 +3,7 @@ import { forwardRef, SVGProps } from "react";
 export const Hamburger = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
   ({ ...props }, ref) => {
     return (
-      <svg viewBox="0 0 32 32 " {...props}>
+      <svg ref={ref} viewBox="0 0 32 32 " {...props}>
         <path d="M4 10h24a2 2 0 0 0 0-4H4a2 2 0 0 0 0 4zm24 4H4a2 2 0 0 0 0 4h24a2 2 0 0 0 0-4zm0 8H4a2 2 0 0 0 0 4h24a2 2 0 0 0 0-4z" />
       </svg>
     );
