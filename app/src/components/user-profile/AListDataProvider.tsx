b388cf7293d86f1f51d9131aa3bed09df75f262e
timthedev07 <timpersonal07@gmail.com>
2023-12-26 13:30:51 +0100

    add contextual data provider to prevent refetch

---
+++ app/src/components/user-profile/AListDataProvider.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { inferProcedureOutput } from "@trpc/server";
+import React, { PropsWithChildren, useContext } from "react";
+import { getUserAnswers } from "../../server/crud/answers/getUserAnswers";
+import { trpc } from "../../trpc/client";
+
+export type Status = "error" | "success" | "loading";
+
+interface AListDataContextType {
+  data?: inferProcedureOutput<typeof getUserAnswers>;
+  status: Status;
+}
+const AListDataContext = React.createContext<AListDataContextType>({
+  status: "loading",
+});
+
+export const useAListData = () => {
+  return useContext(AListDataContext);
+};
+
+export const AListDataProvider: React.FC<
+  PropsWithChildren<{ uid?: string | null }>
+> = ({ children, uid }) => {
+  const { data, status } = trpc.getUserAnswers.useQuery(
+    { uid: uid! },
+    { enabled: !!uid }
+  );
+
+  const value: AListDataContextType = {
+    data,
+    status,
+  };
+
+  return (
+    <AListDataContext.Provider value={value}>
+      {children}
+    </AListDataContext.Provider>
+  );
+};
