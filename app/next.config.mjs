4e1b196acb1022c98a98dc038f7c5257da094bb6
timthedev07 <timpersonal07@gmail.com>
2023-12-27 01:24:35 +0100

    install and init nextmdx

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,9 +1,13 @@
+const withMDX = require("@next/mdx")({
+  options: { remarkPlugins: [], rehypePlugins: [] },
+});
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
     remotePatterns: [
       `${process.env.S3_CLOUDFRONT_DOMAIN}`,
@@ -15,4 +19,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = withBundleAnalyzer(withMDX(nextConfig));
