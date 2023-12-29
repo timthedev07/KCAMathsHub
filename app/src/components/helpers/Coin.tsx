49036d22ea28517c95c077d0f9c6b82643f358e9
timthedev07 <timpersonal07@gmail.com>
2023-12-29 01:49:04 +0100

    fucking fix coin

--- app/src/components/helpers/Coin.tsx
+++ app/src/components/helpers/Coin.tsx
@@ -1,7 +1,36 @@
-import Image from "next/image";
 import { FC } from "react";
-import coinSrc from "../../../public/coin.svg";
 
 export const Coin: FC<{ height: number; width: number }> = (props) => {
-  return <Image alt="coin" src={coinSrc} {...props} />;
+  return (
+    <svg
+      {...props}
+      cache-id="0e816a05d34142ac9c4c73c7b8e35a02"
+      id="e69MPVENRun1"
+      viewBox="0 0 300 300"
+      shapeRendering="geometricPrecision"
+      textRendering="geometricPrecision"
+    >
+      <ellipse
+        rx="126.031637"
+        ry="126.031637"
+        transform="matrix(1.190177 0 0 1.190177 150 150)"
+        fill="#1b3f88"
+        strokeWidth="0"
+      />
+      <text
+        dx="0"
+        dy="0"
+        fontFamily="Lora"
+        fontSize="15"
+        fontWeight="600"
+        transform="matrix(14.197335 0 0 14.197088 75.242144 227.862155)"
+        fill="#fff"
+        strokeWidth="0"
+      >
+        <tspan y="0" fontWeight="600" strokeWidth="0">
+          K
+        </tspan>
+      </text>
+    </svg>
+  );
 };
